13f2fcf2937f226d709d9ebbaf42f8a86e9e51d7
Manish8000pro <manishkumawat6227@gmail.com>
2025-02-18 15:42:35 +0530

    djfka

---
+++ if.cpp
@@ -0,0 +1,44 @@
+
+    //int package;
+   // cin>>package;
+
+   // if(package>10)
+   // {
+   //     cout<<"Accepted";
+  //  }
+   // else
+   // {
+  //      cout<<"rejected";
+
+   //  int marks;
+   //  cout<<"Enter your marks:";
+   //  cin>>marks;
+
+   //  if(marks>33)
+    // {
+    // cout<<"pass";
+   //  }
+   //  else
+   //  {
+   //  cout<<"fail";
+
+   #include<iostream>
+   using namespace std;
+   int main()
+   {
+      int marks;
+      cout<<"Enter your marks:-";
+      cin>>marks;
+      if(marks>33)
+      {
+          cout<<"Result is= "<<"PASS";
+      }
+      else
+      {
+          cout<<"Result is= "<<"FAIL";
+      }
+  
+   }
+  
+      
+  
