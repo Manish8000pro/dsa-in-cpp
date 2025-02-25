1285e1bc58ba30cadf42c4452e0f116fe295697c
Manish8000pro <manishkumawat6227@gmail.com>
2025-02-25 22:35:30 +0530

    aaaaaaaaaaaaaaa

---
+++ C++/pattern_pra.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col,n;
+    {
+        cout<<"Enter the number ";
+        cin>>n;
+
+        for(row=1;row<=n;row=row+1)
+        
+        cout<<"  ";
+
+        for(col=1;col=<2*row-1;col=col+1)
+        
+        cout<<"* ";
+
+        cout<<endl;
+    }
+}
