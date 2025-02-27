b9a68e7c9ace87bc9a5e5adaf3e881925b3aaa5f
Manish8000pro <manishkumawat6227@gmail.com>
2025-02-27 15:46:40 +0530

    Question updated

--- 3_ADV_pattern.cpp
+++ 3_ADV_pattern.cpp
@@ -6,7 +6,8 @@ int main()
     cout<<"Enter the input; ";
     cin>>n;
     for(row=1;row<=n;row=row+1)
-    {
+    {  //print space
+        
         for(col=1;col<=n-row;col=col+1)
         cout<<" ";
 
