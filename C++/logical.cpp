b4ffbef5b16547868335fc40ee9d425c3017b92f
Manish8000pro <manishkumawat6227@gmail.com>
2025-03-29 22:12:15 +0530

    new Q. add

---
+++ C++/logical.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    // logical oprator 
+    
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // if(a>b&&a>c)
+    // cout<<"yes";
+    // else
+    // cout<<"NO;
+    //or
+
+    char name=c;
+    
+    
+}
---
+++ C++/oprator.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a=10;
+
+    // cout<<a++;
+    // cout<<endl;
+    // cout<<a;
+
+    // int a=10;
+    // cout<<++a;
+    // cout<<endl;
+    // cout<<a;
+
+    //comparision oprator 00
+
+    // int a,b;
+    // cout<<"Enter the value of a ";
+    // cin>>a;
+    // cout<<"Enter the value of b ";
+    // cin>>b;
+
+    // if(a==b)
+    // cout<<"Yes ";
+    // else
+    // cout<<"No ";
+
+   // <  ,>
+    int a,b;
+    cout<<"Enter the value of a ";
+    cin>>a;
+    cout<<"Enter the value of b ";
+    cin>>b;
+
+    if(a>b)
+    cout<<"Yes ";
+    else
+    cout<<"No ";
+
+
+
+
+
+}
