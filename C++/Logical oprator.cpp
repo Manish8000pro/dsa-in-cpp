5b77377be9effb25829e2f59f93027d9cc4b628c
Manish8000pro <manishkumawat6227@gmail.com>
2025-04-01 14:32:11 +0530

    add

---
+++ C++/Logical oprator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+    // logical oprator
+
+//     int a,b,c;
+//      cin>>a>>b>>c;
+//    if(a>b&&a>c)
+//    cout<<"Yes";
+//    else 
+//    cout<<"NO";
+
+//or
+
+char name='o';
+
+if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+cout<<"vovel";
+else
+cout<<"consonant";
+
+}
