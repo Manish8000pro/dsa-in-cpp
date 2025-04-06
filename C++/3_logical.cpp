19ae2e7ab98acdc79f98c5bee3b1b35c2717b993
Manish8000pro <manishkumawat6227@gmail.com>
2025-04-06 12:31:50 +0530

    bitwise oprator

---
+++ C++/3_logical.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Bitwise oprator
+    //& oprator
+
+    // int ans=4&5;
+    // cout<<ans<<endl;
+    //or oprator 
+
+    // int ans=2|2;
+    // cout<<ans<<endl;
+    // ex-or oprator
+
+    int ans=2^3;
+    cout<<"my ans is "<<ans<<endl;
+
+}
