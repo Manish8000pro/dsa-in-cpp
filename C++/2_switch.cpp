3f97fbdcfe1c6d6cca454bb9978f399a50d91649
Manish8000pro <manishkumawat6227@gmail.com>
2025-04-15 22:22:59 +0530

    switch add

---
+++ C++/2_switch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    // int i;
+    // cout<<"Enter the number ";
+    // cin>>i;
+    // switch(i)
+    // {
+    //     case 1:
+    //     cout<<"Rohit";
+    //     break;
+        
+    //     case 2:
+    //     cout<<"Mohit";
+    //     break;
+
+    //     default:
+    //     cout<<"sahil";
+
+
+    // }
+
+
+    int i;
+    cout<<"Enter the number ";
+    cin>>i;
+    switch(i)
+    case 1:
+    
+
+}
