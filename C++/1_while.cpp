d6c10f8c6b9caa6eac94ab97da6dca14a73c8e14
Manish8000pro <manishkumawat6227@gmail.com>
2025-04-15 22:19:40 +0530

    while add

---
+++ C++/1_while.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int n;
+    // cout<<"Enter the number";
+    // cin>>n;
+    // int i=1;// initalize 
+
+    // while(i<=n) // Break 
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+// Table 
+    int n;
+    cout<<"Enter the number " ;
+    cin>>n;
+    int i=1;// initalize 
+
+    while(i<=10) // Break 
+    {
+        cout<<i*n<<endl;
+        i++;
+    }    
+}
