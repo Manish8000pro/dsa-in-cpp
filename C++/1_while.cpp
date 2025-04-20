35ac0b67a66eb809cd73bedacf7550d80dd6ec83
Manish8000pro <manishkumawat6227@gmail.com>
2025-04-20 15:19:24 +0530

    sol. new Q.in while

--- C++/1_while.cpp
+++ C++/1_while.cpp
@@ -14,14 +14,26 @@ int main()
     // }
 
 // Table 
-    int n;
-    cout<<"Enter the number " ;
-    cin>>n;
-    int i=1;// initalize 
 
-    while(i<=10) // Break 
-    {
-        cout<<i*n<<endl;
-        i++;
-    }    
+    // int n;
+    // cout<<"Enter the number " ;
+    // cin>>n;
+    // int i=1;// initalize 
+
+    // while(i<=10) // Break 
+    // {
+    //     cout<<i*n<<endl;
+    //     i++;
+    // }    
+
+   int n;
+   cout<<"Enter the number ";
+   cin>>n;
+   int i=1;
+   while(i<=n)
+   {
+    if(n%i==0)
+    cout<<i<<endl;
+    i++;
+   }
 }
