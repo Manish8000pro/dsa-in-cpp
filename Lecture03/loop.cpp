8f45c89c9700470816c3eb0b90c15005bad3b3ca
Manish8000pro <manishkumawat6227@gmail.com>
2025-12-17 15:07:39 +0530

    sum of no. and multiplication and counting

---
+++ Lecture03/loop.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // sum of first n no.
+    // int sum = 0;
+
+    // for(int i=1;i<=10;i++){
+    //    sum = sum+i; 
+    // }
+    // cout<< "Sum:- "<<sum<<endl;
+
+
+    // Multiplication Table
+
+    // int i, n ;
+    // cout<<"Enter the number:- ";
+    // cin>>n;
+    // for(int i = 1;i<=10;i++){
+    //     cout<<n*i<<endl;
+    // }
+
+    // Counting 
+
+    // int i,n ;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // for(int i=1; i<=n; i++){
+    //     cout<<i<<endl;
+    // }
+    
+}
