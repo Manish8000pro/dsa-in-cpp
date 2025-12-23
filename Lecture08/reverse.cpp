adec756a2b20a755e05b730ede676814ccc13a48
Manish8000pro <manishkumawat6227@gmail.com>
2025-12-23 16:04:19 +0530

    reverse array

---
+++ Lecture08/reverse.cpp
@@ -0,0 +1,11 @@
+// https://strikes.in/practice/68ac0c51b68bc4c304e47837
+
+    // Reverse Array 
+
+    // int n = arr.size();
+
+	// for(int i=0;i<n/2;i++){
+	// 	int temp = arr[i];
+	// 	arr[i] = arr[n-1-i];
+	// 	arr[n-1-i] = temp;
+	// }
