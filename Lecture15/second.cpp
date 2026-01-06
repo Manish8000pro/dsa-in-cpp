cf18fd3461844989449f4c7a7b3a217ea2927f27
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-06 17:16:28 +0530

    sqr rt of x

---
+++ Lecture15/second.cpp
@@ -0,0 +1,32 @@
+// 18. Sqrt(x)
+
+int mySqrt(int x) {
+    // Your code here
+
+	if(x<=1){
+		return x;
+	}
+
+	int start = 1,end = x,ans;
+	
+	while(start<=end){
+		int mid = start+(end-start)/2;
+
+		if(mid==x/mid){
+			return mid;
+		}
+		// Move right side 
+		else if(mid<x/mid){
+			ans = mid;
+			start = mid+1;
+			
+		}
+		// Move left side
+		else{
+			end = mid-1;
+			
+		}
+
+	}
+	return ans;
+}
