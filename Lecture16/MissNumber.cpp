c2f48e087b0f7a99cc64fc7969bae0232cd620f4
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-09 10:49:31 +0530

    kth missing number

--- Lecture16/MissNumber.cpp
+++ Lecture16/MissNumber.cpp
@@ -1,13 +1,25 @@
-int findKthPositive(vector<int>& arr, int k) {
-    // Your code here
-	int missingNumber = k;
-	for(int i = 0;i<arr.size();i++){
-		if(arr[i]>missingNumber){
-			return missingNumber;
-		}
-		else{
-			missingNumber++;
-		}
-	}
-	return missingNumber;
-}
+// int findKthPositive(vector<int>& arr, int k) {
+//     // Your code here
+// 	int missingNumber = k;
+// 	for(int i = 0;i<arr.size();i++){
+// 		if(arr[i]>missingNumber){
+// 			return missingNumber;
+// 		}
+// 		else{
+// 			missingNumber++;
+// 		}
+// 	}
+// 	return missingNumber;
+
+		// SECOND ANSWER
+	// 	int n = arr.size();
+
+	// for(int i=0;i<n;i++){
+	// 	int totalMissing = arr[i] - (i+1);
+
+	// 	if(totalMissing>=k){
+	// 		return i+k;
+	// 	}
+	// }
+	// return n+k;
+// }
