82e6723cd4537dbf7b0d31a23eaf47f67da0ab84
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-10 20:48:34 +0530

    allocate the minimum numberr

---
+++ Lecture17/Allocatemini.cpp
@@ -0,0 +1,40 @@
+bool allocateBook(int maxPages,vector<int> &arr, int k){
+
+	// count: number of students, Pages : unko kitne pages mile h
+
+	int count = 1, pages = arr[0];
+	for(int i=1;i<=arr.size();i++){
+		pages+=arr[i];
+		if(pages>maxPages){
+			count++;
+			pages = arr[i];
+		}
+	}
+
+	return count == k;
+}
+
+
+
+int findPages(vector<int> &arr, int k) {
+    // Your code here
+
+	int n = arr.size();
+
+	if(k>n){
+		return -1;
+	}
+	// Array ka jo maximum hoga
+	int start =0;
+	for(int i = 0; i<n;i++){
+		start = Max(start,arr[i])
+	}
+
+	while(start){
+		if (allocateBook(start,arr,k)){
+			return start;
+		}
+		start++;
+	}
+}
+
