6972295043f63c2786366e4703ca8b9bbe7e9a3c
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-21 21:42:50 +0530

    second Solution of trapping rain water

--- Lecture21/trappingfirst.cpp
+++ Lecture21/trappingfirst.cpp
@@ -33,3 +33,39 @@ int trapWater(vector<int>& arr) {
 
 
 }
+
+// Second solution
+
+int trapWater(vector<int>& arr) {
+
+	int n = arr.size();
+
+	vector<int>RightMax(n,0);
+
+	RightMax[n-1] = arr[n-1];
+
+	int LeftMax = arr[0];
+    
+
+	// Suffix Max
+
+	for(int i=n-2;i>0;i--){
+       RightMax[i] += max(RightMax[i+1],arr[i]);
+	}  
+
+
+	int totalWater = 0;
+
+	for(int i=1;i<n-1;i++){
+		int minLeftRight = min(LeftMax,RightMax[i+1]);
+		if(minLeftRight>arr[i]){
+			totalWater+=minLeftRight-arr[i];
+		}
+
+		LeftMax = max(LeftMax,arr[i]);
+	}
+
+	return totalWater;
+
+
+}
