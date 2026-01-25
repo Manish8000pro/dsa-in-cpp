528aa47c797c4d81a301f8a3f6357aef3669c6e9
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-25 21:55:29 +0530

    spiral matrix

---
+++ Lecture23/spiral.cpp
@@ -0,0 +1,36 @@
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int>spiral;
+
+	int rowStart = 0, colStart = 0,rowEnd = matrix.size()-1,colEnd = matrix[0].size();
+
+	while(rowStart<=rowEnd&&colStart<=colEnd){
+		//first row ko print kravo 
+
+		for(int j= colStart;j<=colEnd;j++){
+			spiral.push_back(matrix[rowStart][j]);
+		}
+		rowStart++;
+
+		// Last column ko print kravo
+
+		for(int i = rowStart;i<=rowEnd;i++){
+			spiral.push_back(martix[i][colEnd]);
+		}
+		colEnd--;
+
+		// Last row ko print kravo 
+
+		for(int j =colEnd;j>=colStart;j--){
+			spiral.push_back(matrix[rowEnd][j]);
+		}
+		rowEnd--;
+
+		// first col ko print kravo
+
+		for(int i = rowEnd;i>=rowStart;i--){
+			spiral.push_back(matrix[i][colStart]);
+		}
+		colStart++;
+	}
+	return spiral;
+}
