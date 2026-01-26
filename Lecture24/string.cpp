bbaa2dcbadff3b987911033af143c2f7bc5f16fe
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-26 23:23:34 +0530

    learning

--- Lecture24/string.cpp
+++ Lecture24/string.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s = "Manish";
-    cout<<s<<endl;
+    // string s = "Manish";
+    // cout<<s<<endl;
+
+    string c = "a";
+    cout<<c+"2"<<endl;
 
 }
