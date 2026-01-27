4956a3918d6545f19e1e53259c0a0fb1d87746ea
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-27 23:32:57 +0530

    image rotation

---
+++ Lecture23/rotateimage.cpp
@@ -0,0 +1,21 @@
+void rotate(vector<vector<int>>& matrix) {
+    int rowSize = matrix.size(),colSize = matrix[0].size();
+
+	for(int i = 0;i<rowSize-1;i++){
+		for(int j = i+1;j<colSize;j++){
+			swap(matrix[i][j],matrix[j][i]);
+		}
+	}
+
+	// vertically flip kr do 180 degree rotate kr do 
+
+	int colStart = 0, colEnd = matrix[0].size()-1;
+
+	while(colStart<colEnd){
+		for(int i = 0;i<row.size;i++){
+			swap(matrix[i][colStart],matrix[i][colEnd]);
+		}
+		colStart++,colEnd--;
+	}
+	
+}
