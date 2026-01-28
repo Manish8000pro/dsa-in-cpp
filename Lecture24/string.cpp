e0c25c1c6d7f5f52b546a8a734e046e97851890b
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-28 18:22:20 +0530

    push back and pop back

--- Lecture24/string.cpp
+++ Lecture24/string.cpp
@@ -11,12 +11,30 @@ int main(){
     // string c = "a";
     // cout<<c+"2"<<endl;
 
-    string s1;
+    // string s1;
 
-    cin>>s1;
+    // cin>>s1;
 
     // getline(cin,s1);
-    cout<<s1;
+    // cout<<s1;
+
+    string s1 = "Manish";
+    string s2 = "Dinesh";
+
+    // add string 
+
+    string s3 = s1+s2;
+
+    // cout<<s3;
+
+    // kisi charecter ko push bhi kr sakta hu
+
+    s3.push_back('R');
+    // cout<<s3;
+
+    s3.pop_back();
+
+    cout<<s3;
 
     return 0;
 
