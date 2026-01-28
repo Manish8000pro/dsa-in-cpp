4d012f7731816f9d380d9a88f10710042e75d609
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-28 18:12:46 +0530

    getline for the full name and space writning

--- Lecture24/string.cpp
+++ Lecture24/string.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s = "Manish";
-    cout<<s<<endl;
+    // string s = "Manish";
+    // cout<<s<<endl;
 
-    cout<<s.size();
-    cout<<endl;
-    cout<<s.length();
+    // cout<<s.size();
+    // cout<<endl;
+    // cout<<s.length();
 
     // string c = "a";
     // cout<<c+"2"<<endl;
+
+    string s1;
+
+    cin>>s1;
+
+    // getline(cin,s1);
+    cout<<s1;
 
     return 0;
 
