7e3db91a7add54747a563c1fdb8fa7b24c0b4900
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-29 15:10:10 +0530

    defancing ip address

---
+++ Lecture24/Defanging an IP Address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        //Code here
+
+		string ans;
+
+		for(int i = 0;i<=address.size()-1;i++){
+			if(address[i]=='.'){
+				ans = ans +"[.]";
+			}
+			else{
+				ans.push_back(address[i]);
+			}
+		}
+		return ans;
+    }
+
+	
+};
