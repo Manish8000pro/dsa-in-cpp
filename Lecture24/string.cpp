a6a606e2975afee1838a03d3f8e71237faac6950
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-29 12:48:28 +0530

    learn the advancement in string

--- Lecture24/string.cpp
+++ Lecture24/string.cpp
@@ -18,23 +18,31 @@ int main(){
     // getline(cin,s1);
     // cout<<s1;
 
-    string s1 = "Manish";
-    string s2 = "Dinesh";
+    // string s1 = "Manish";
+    // string s2 = "Dinesh";
 
     // add string 
 
-    string s3 = s1+s2;
+    // string s3 = s1+s2;
 
     // cout<<s3;
 
     // kisi charecter ko push bhi kr sakta hu
 
-    s3.push_back('R');
+    // s3.push_back('R');
     // cout<<s3;
 
-    s3.pop_back();
+    // s3.pop_back();
 
-    cout<<s3;
+    // cout<<s3;
+
+    // char c = 'a'+5;
+    // cout<<c;
+
+    // Advancement 
+
+        // char c[6] = "Rohit";
+        // cout<<c;
 
     return 0;
 
