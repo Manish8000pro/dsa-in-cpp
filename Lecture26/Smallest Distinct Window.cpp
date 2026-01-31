a10671389db85eb12f8332edf81037fa6a0cb862
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-31 15:43:57 +0530

    smallest distinct window problem solved

---
+++ Lecture26/Smallest Distinct Window.cpp
@@ -0,0 +1,37 @@
+int findSubString(string str) {
+    // Your code here
+	int uniqueChar = 0,n=str.size();
+	bool freq[256]={0};
+
+	for(int i=0;i<n;i++){
+		if(freq[str[i]]==0){
+			freq[str[i]]=1;
+			uniqueChar++;
+		}
+	}
+
+	int start=0,end=0,answer=str.size(),currentUniqueChar = 0;
+	int count[256]={0};
+
+	while(end<n){
+		count[str[end]]++;
+
+		if(count[str[end]]==1){
+			currentUniqueChar++;
+		}
+
+		// currentUniqueChar equal to uniqueChar
+
+		while(currentUniqueChar==uniqueChar){
+			answer = min(answer,end-start+1);
+			count[str[start]]--;
+			if(count[str[start]]==0){
+				currentUniqueChar--;
+			}
+			start++;
+		}
+		end++;
+	}
+	return answer;
+
+}
