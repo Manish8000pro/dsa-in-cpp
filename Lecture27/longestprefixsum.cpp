65a31d27e69c84f59bd12b8fb54586411b637fc6
Manish8000pro <manishkumawat6227@gmail.com>
2026-02-03 16:21:32 +0530

    Problem solved LPS

--- Lecture27/longestprefixsum.cpp
+++ Lecture27/longestprefixsum.cpp
@@ -1,1 +1,22 @@
 // Longest prefix sum 
+int n = s.size();
+
+	vector<int>LPS(n,0);
+
+	int prefix = 0,suffix=1;
+
+	while(suffix<n){
+		if(s[prefix]==s[suffix]){
+			LPS[suffix]=prefix+1;
+			prefix++,suffix++;
+		}
+		else{
+			if(prefix==0){
+				suffix++;
+			}
+			else{
+				prefix = LPS[prefix-1];
+			}
+		}
+	}
+	return LPS[n-1];
