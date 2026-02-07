04b5df6d61a0f6e4bc67db966865a4d5c565d7c8
Manish8000pro <manishkumawat6227@gmail.com>
2026-02-07 20:25:40 +0530

    solution

---
+++ Lecture28/multiplyothers.cpp
@@ -0,0 +1,19 @@
+vector<int> productExceptSelf(vector<int>& nums) {
+    // code here
+	int n = nums.size();
+	vector<int>mul(n,1);
+
+	int prefix = 1;
+
+	for(int i=0;i<n;i++){
+		mul[i]=prefix;
+		prefix*=nums[i];
+	}
+
+	int suffix = 1;
+	for(int i=n-1;i>=0;i--){
+		mul[i]*=suffix;
+		suffix*=nums[i];
+	}
+	return mul;
+}
