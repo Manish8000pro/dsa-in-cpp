91189f3baff1b8ee05101a8b69a1e6bbbd344907
Manish8000pro <manishkumawat6227@gmail.com>
2026-02-09 12:42:29 +0530

    array making

--- Lecture29/start.cpp
+++ Lecture29/start.cpp
@@ -3,19 +3,35 @@ using namespace std;
 
 int main()
 {
-    int a = 20;
-    int *ptr = &a;
+    // int a = 20;
+    // int *ptr = &a;
 
-    // Ptr is a pointer its point to integer type value
+    // // Ptr is a pointer its point to integer type value
 
-    cout<<a<<endl;
-    // value of a
-    cout<<&a<<endl;
-    // Address of a
-    cout<<ptr<<endl;
-    // Address of a
-    cout<<&ptr<<endl;
-    // Address of Ptr
+    // cout<<a<<endl;
+    // // value of a
+    // cout<<&a<<endl;
+    // // Address of a
+    // cout<<ptr<<endl;
+    // // Address of a
+    // cout<<&ptr<<endl;
+    // // Address of Ptr
+
+
+    int arr[5] = {1,2,5,8,10};
+    for(int i=0;i<5;i++){
+        cout<<arr+i<<endl;
+    }
+
+    // Print the value 
+    for(int i=0;i<5;i++){
+        cout<<*(arr+i)<<endl;
+    }
+
+    int *p = arr;
+    for(int i=0;i<5;i++){
+        cout<<p[i] <<endl;
+    }
 
     return 0;
 }
