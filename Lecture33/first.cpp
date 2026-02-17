3120ec4bd4260e60d5ccad8f3c1cc32118f1d5d4
Manish8000pro <manishkumawat6227@gmail.com>
2026-02-17 13:21:05 +0530

    array printing

--- Lecture33/first.cpp
+++ Lecture33/first.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 using namespace std;
 
+void print(int arr[],int index,int n){
+
+    // base condition
+    if(index==n){
+        return;
+    }
+
+    // print krna h 
+    cout<<arr[index]<<endl;
+
+     print(arr,index+1,n);
+}
+
 int main(){
 
-    // array reversing 
+   int arr[10] = {1,2,3,5,6,7,8,9,10,11};
+
+    print(arr,0,10);
+    return 0;
 }
