1dc355030e57bd0e66332e7fa3d09b04ee291935
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-03 21:31:14 +0530

    combinationII

---
+++ Lecture38/combinationII.cpp
@@ -0,0 +1,34 @@
+void combination(vector<int>&nums,int index,int n,vector<int>&path,vector<vector<int>>&ans,int target){
+
+	if(target==0){
+		ans.push_back(path);
+		return;
+	}
+
+	if(target<0 || index==n){
+		return;
+	}
+
+
+	vector<bool>used(51,0);
+
+	for(int i = index;i<n;i++){
+		if(!used[nums[i]]){
+			used[nums[i]]=1;
+			path.push_back(nums[i]);
+			combination(nums,i+1,n,path,ans,target-nums[i]);
+			path.pop_back();
+		}
+	}
+}
+
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    
+	vector<vector<int>>ans;
+	vector<int>path;
+	
+
+	combination(candidates,0,candidates.size(),path,ans,target);
+	return ans;
+}
