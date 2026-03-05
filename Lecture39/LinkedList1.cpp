8586009cf3d92cae2fa1334a126ea9c36359daa1
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-05 11:08:51 +0530

    Node creation

--- Lecture39/LinkedList1.cpp
+++ Lecture39/LinkedList1.cpp
@@ -7,9 +7,9 @@ class Node{
     Node *next; 
 
     // constructer/
-    Node(int value,Node*addrr){
+    Node(int value){
         data = value;
-        next = addrr;
+        next = NULL;
     }
 };
 
@@ -22,8 +22,35 @@ int main(){
 
     // cout<<N1.data<<" ";
 
-    Node*head = new Node(10,NULL);
-    cout<<head->data<<" ";
+    // Node*head = new Node(10,NULL);
+    // cout<<head->data<<" ";
+
+    int arr[5] = {10,12,20,40,50};
+    Node *Head = NULL, *Tail = NULL;
+
+
+    // Create first node 
+    for(int i=0;i<5;i++){
+    if(!Head){
+        // first Node Creation
+        Head = new Node(arr[0]);
+        Tail = Head;
+    }
+    else{
+        // Other node creation by adding element Last
+        Tail->next = new Node(arr[i]);
+        Tail = Tail->next;
+    }
+    }
+
+    // travese the linkedlist
+
+    Tail = Head;
+
+    while(Tail!=NULL){
+        cout<<Tail->data<<" ";
+        Tail = Tail->next;
+    }
 
     return 0;
 
