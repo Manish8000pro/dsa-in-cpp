0e2027d6d7eb1dfc66d0e89b61257c419b84b80d
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-09 21:50:05 +0530

    Question solved

---
+++ Practice Questions/Circular Linked List.cpp
@@ -0,0 +1,26 @@
+/*
+struct ListNode {
+    int data;
+    ListNode* next;
+    ListNode(int x) : data(x), next(nullptr) {}
+};
+*/
+bool isCircular(ListNode *head) {
+    if(!head || !head->next){
+		return 0;
+	}
+
+	ListNode *temp = NULL;
+	temp = head->next;
+
+	while(temp&&temp!=head){
+		temp = temp->next;
+	};
+
+	if(temp==head){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
