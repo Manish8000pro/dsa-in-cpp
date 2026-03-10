169c1e8e7ebc726648b5fb75004e8d2c6b807b5b
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-10 23:28:44 +0530

    other sol.

--- Practice Questions/Circular Linked List.cpp
+++ Practice Questions/Circular Linked List.cpp
@@ -26,3 +26,26 @@ bool isCircular(ListNode *head) {
 }
 
 // Oteher solution of Circular LinkedList
+
+/*
+struct ListNode {
+    int data;
+    ListNode* next;
+    ListNode(int x) : data(x), next(nullptr) {}
+};
+*/
+bool isCircular(ListNode *head) {
+    if(!head){
+		return 0;
+	}
+
+	ListNode* Temp = head->next;
+
+	while(Temp&&Temp!=head){
+		Temp = Temp->next;
+	}
+	if(!Temp){
+		return 0;
+	}
+	return 1;
+}
