3b5eba40c4e5ae5ffb62ea8d5e0ef8568d7c2358
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-12 23:32:17 +0530

    insertion In DLL

---
+++ Lecture41/DLL2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+
+    int val;
+    Node* next, *prev;
+
+    // constructer
+
+    Node(int value){
+        val = value;
+        next = prev = NULL;
+    }
+};
+
+void printDLL(Node *head){
+    while (head)
+    {
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    
+}
+
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+    Node *head = NULL,*tail =NULL ;
+
+    for(int i=0;i<5;i++){
+        if(!head){
+            head = new Node(arr[i]);
+            tail = head;
+        }
+        else{
+            Node *temp = new Node(arr[i]);
+            temp->prev = tail;
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    
+
+    // traverse : print the list 
+
+    printDLL(head);
+
+    return 0;
+}
