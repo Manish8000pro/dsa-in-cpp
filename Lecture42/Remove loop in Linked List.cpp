7f8a1c1db3fc642d63b8c5bfa74b2c1e954aa26c
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-15 22:47:57 +0530

    Remove loop linkedList

---
+++ Lecture42/Remove loop in Linked List.cpp
@@ -0,0 +1,48 @@
+/* struct Node {
+    int data;
+    Node* next;
+    Node(int x) : data(x), next(nullptr) {}
+}; */
+
+void removeLoop(Node* head) {
+
+	if(!head||!head->next){
+		return;
+	}
+
+
+    Node* slow = head , *fast = head,*prev = NULL;
+
+	while(fast&&fast->next){
+		prev = slow;
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if(slow==fast){
+			break;
+		}
+	}
+
+	// loop nhi ho
+
+	if(slow!=fast){
+		return;
+	}
+
+	if(slow==head){
+		while(fast->next!=head){
+			fast = fast->next;
+		}
+		fast->next = NULL;
+		return;
+	}
+
+	slow = head;
+
+	while(slow->next!=fast->next){
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	fast->next = NULL;
+}
