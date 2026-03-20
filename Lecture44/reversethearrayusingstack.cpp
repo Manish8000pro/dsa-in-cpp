ec653383f018af6380069de0c48dc499db872e42
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-20 12:59:11 +0530

    code of stack

--- Lecture44/reversethearrayusingstack.cpp
+++ Lecture44/reversethearrayusingstack.cpp
@@ -1,1 +1,16 @@
 // stack problem solving with stack 
+void reverseArray(vector<int>& arr) {
+
+    stack<int>s;
+
+    for(int i=0;i<arr.size();i++){
+        s.push(arr[i]);
+    }
+
+    // pop the element of stack and push them into the array
+
+    for(int i=0;i<arr.size();i++){
+        arr[i] = s.top();
+        s.pop();
+    }
+}
