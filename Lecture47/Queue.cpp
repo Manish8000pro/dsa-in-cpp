d562fe05b9a4daa64ff471ba8ff9267c4de62296
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-24 18:55:59 +0530

    writing and learning the Queue code

--- Lecture47/Queue.cpp
+++ Lecture47/Queue.cpp
@@ -7,7 +7,7 @@ class Queue{
     int *arr;
 
     Queue(int size){
-        arr= new int size[];
+        arr= new int [size];
         capacity = size;
         front = rear = -1;
     }
@@ -38,18 +38,52 @@ class Queue{
             return;
         }
     }
-}
-
+    
     void pop(){
         if(isEmpty()){
             cout<<"Queue is empty\n";
             return;
         }
         else{
+            cout<<arr[front]<<" popped from the Queue\n";
             front = front+1;
+
+            // if front becomes the greater then rear
+
+            if(front>rear){
+                front = rear = -1;
+            }
         }
     }
 
-int main(){
+    int getFront(){
+            if(isEmpty()){
+                cout<<"Queue is empty\n";
+                return -1;
+            }
+            return arr[front];
+        }
 
+        int isSize(){
+            if(isEmpty()){
+                return 0;
+            }
+            return rear-front+1;
+        }
+};
+
+    
+
+int main(){
+    Queue q(5);
+    q.push(20);
+    q.push(30);
+    q.pop();
+    cout<<q.isSize()<<endl;
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+
+    return 0;
 }
