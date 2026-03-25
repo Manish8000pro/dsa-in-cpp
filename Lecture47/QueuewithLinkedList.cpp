eb80449fd1c016e76a6b623348d2af0ba3d2785e
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-25 13:29:41 +0530

    Queue with LinkedList

--- Lecture47/QueuewithLinkedList.cpp
+++ Lecture47/QueuewithLinkedList.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class Node(){
+class Node{
     public:
     int data;
     Node *next;
@@ -13,6 +13,7 @@ class Node(){
 };
 
 class Queue{
+    public:
     Node *front , *rear;
     int size;
 
@@ -22,18 +23,61 @@ class Queue{
     }
 
     bool isEmpty(){
-        retrun size==0;
+        return size==0;
     }
 
     void push(int x){
         if(isEmpty()){
             front = rear= new Node(x);
             cout<<x<<" Pushed into Queue\n";
+            size++;
+            return;
+        }
+        else{
+            rear->next = new Node(x);
+            rear = rear->next;
+            cout<<x<<" Pushed into Queue\n";
+            size++;
             return;
         }
     }
-}
+
+    void pop(){
+        if(isEmpty()){
+            cout<<" Queue is empty\n ";
+        }
+        else{
+            Node* temp = front;
+            size--;
+            front = front->next;
+            cout<<temp->data<<" Popped from Queue\n";
+            delete temp;
+            return;
+        }
+    }
+
+    int getFront(){
+        if(isEmpty()){
+            cout<<"Queue is Empty\n";
+            return -1;
+        }
+        else{
+            return front->data;
+        }
+    }
+
+    int isSize(){
+        return size;
+    }
+
+};
+
+    
 
 int main(){
-
+    Queue q;
+    q.push(10);
+    q.push(20);
+    q.pop();
+    cout<<q.isSize()<<endl;
 }
