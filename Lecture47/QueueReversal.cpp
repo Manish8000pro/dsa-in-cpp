d6fee7391f7926583d9f8a74c04cabdcc4e6df2d
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-26 16:16:58 +0530

    added the lect.47

---
+++ Lecture47/QueueReversal.cpp
@@ -0,0 +1,13 @@
+queue<int> reverseQueue(queue<int> q) {
+    stack<int>s;
+
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    return q;
+}
