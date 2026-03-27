09b8476c3a0a77f78d9405c33d9526cac5f13205
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-27 17:51:49 +0530

    problem solved \\ error resolved

--- Lecture48/First negative in every window of size k.cpp
+++ Lecture48/First negative in every window of size k.cpp
@@ -1,25 +1,30 @@
 vector<int> firstNegativeWindow(vector<int>& arr, int k) {
+    
     vector<int>result;
     queue<int>q;
     int n = arr.size();
+    // first insert first k-1 element into our queue
 
-    for(i=0;i<k-1;i++){
+    for(int i=0;i<k-1;i++){
         if(arr[i]<0){
             q.push(i);
         }
     }
 
-    for(i=k-1;i<n;i++){
-        //if elements is negetive push its index
+
+    for(int i=k-1;i<n;i++){
+        // if element is negative, push its index
         if(arr[i]<0){
             q.push(i);
         }
 
-        // should we delete the  front element of queue because it can be out of our window size 
-
+        // should we delete the front element of queue because it can be out of our current window size
         if(!q.empty()&&(q.front()<=i-k)){
             q.pop();
         }
+
+
+        // if q.front contain any index, push its value into our array, else push 0 into array(result)
 
         if(q.empty()){
             result.push_back(0);
@@ -27,6 +32,12 @@ vector<int> firstNegativeWindow(vector<int>& arr, int k) {
         else{
             result.push_back(arr[q.front()]);
         }
+
     }
+
+
     return result;
+
+
+    
 }
