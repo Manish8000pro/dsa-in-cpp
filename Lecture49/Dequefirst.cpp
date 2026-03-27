b40017da77443b7c6ed32551d5fd8c6d5fd83f09
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-27 22:30:09 +0530

    Uderstand and write the code of Deque

--- Lecture49/Dequefirst.cpp
+++ Lecture49/Dequefirst.cpp
@@ -24,7 +24,7 @@ class Deque{
     }
 
     bool isEmpty(){
-        return size = 0;
+        return size == 0;
     }
 
     // Pushback
@@ -34,11 +34,12 @@ class Deque{
             size++;
         }
         else{
-            rear->next new Node(val);
+            rear->next = new Node(val);
             rear->next->prev = rear;
             rear = rear->next;
             size++;
         }
+        cout<<val<<" pushBack into Deque\n";
     }
     // pushfront
     void pushFront(int val){
@@ -52,11 +53,87 @@ class Deque{
             front = front->prev;
             size++;
         }
+         cout<<val<<" pushFront into Deque\n";
     }
     // popback
+    void popBack(){
+        if(isEmpty()){
+            cout<<" Deque is empty\n";
+            return;
+        }
+        else{
+            // single element 
+            if(rear==front){
+                cout<<rear->data<<" popped back from Deque\n";
+                delete rear;
+                rear = front = NULL;
+                size=0;
+            }
+            else{
+                cout<<rear->data<<" popped back from Deque\n";
+                rear = rear->prev;
+                delete rear->next;
+                rear->next = NULL;
+                size--;
+            }
+        }
+    }
     // popfront
-}
+    void popFront(){
+        if(isEmpty()){
+            cout<<" Deque is empty\n";
+            return;
+        }
+        else{
+            // single element 
+            if(rear==front){
+                cout<<front->data<<" popped front from Deque\n";
+                delete rear;
+                rear = front = NULL;
+                size--;
+            }
+            else{
+                cout<<front->data<<" popped front from Deque\n";
+                front = front->next;
+                delete front->prev;
+                front->prev = NULL;
+                size--;
+            }
+        }
+    }
+
+    int getFront(){
+        if(isEmpty()){
+            return -1;
+        }
+        return front->data;
+    }
+
+    int getBack(){
+        if(isEmpty()){
+            return -1;
+        }
+        return rear->data;
+    }
+};
 
 int main(){
+    Deque D;
+    D.pushBack(10);
+    D.pushBack(20);
+    D.pushBack(55);
+    D.pushFront(40);
+    D.pushBack(21);
+    D.pushFront(74);
+
+    cout<<D.size<<endl;
+    cout<<D.getBack()<<endl;
+    cout<<D.getFront()<<endl;
+
+    D.popBack();
+    D.popFront();
+      cout<<D.size<<endl;
+    cout<<D.getBack()<<endl;
+    cout<<D.getFront()<<endl;
 
 }
