be5193805b5592819b98ac1c4c2e2790c0ef650e
Manish8000pro <manishkumawat6227@gmail.com>
2026-03-30 14:04:24 +0530

    second sol of the problem Get Min from stack

--- Lecture50/Get Min from Stack.cpp
+++ Lecture50/Get Min from Stack.cpp
@@ -41,3 +41,56 @@ class SpecialStack {
         return st.empty();
     }
 };
+
+// ---------------------SECOND SOLUTION-----------------------------------------------------
+
+class SpecialStack {
+  public:
+  stack<long long>st;
+    void push(int x) {
+        // code here
+        if(st.empty()){
+            long long a = x;
+            st.push(a*1000000001+a);
+        }
+        else{
+            
+            long long minimumElement = st.top()%1000000001;
+            long long a=x;
+            minimumElement = min(minimumElement,a);
+            st.push(a*1000000001+minimumElement);
+
+        }
+    }
+    void pop() {
+        // code here
+        if(st.empty()){
+            return;
+        }
+        else{
+            st.pop();
+        }
+    }
+    int peek() {
+        // code here
+        if(st.empty()){
+            return -1;
+        }
+        long long element = st.top()/1000000001;
+        int el = element;
+        return el;
+    }
+    int getMin() {
+        // code here
+        if(st.empty()){
+            return -1;
+        }
+        long long element = st.top()%1000000001;
+        int el = element;
+        return el;
+    }
+    bool isEmpty() {
+        // code here
+        return st.empty();
+    }
+};
