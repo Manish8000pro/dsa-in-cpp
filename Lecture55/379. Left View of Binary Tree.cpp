a818115907256d2970675d9f7d0323c505e19aa8
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-10 15:01:55 +0530

    379 problem solved

---
+++ Lecture55/379. Left View of Binary Tree.cpp
@@ -0,0 +1,37 @@
+/*
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    void Lview(Node* root,vector<int>&ans,int level){
+        if(!root){
+            return;
+        }
+        if(ans.size()==level){
+            ans.push_back(root->data);
+        }
+
+        Lview(root->left,ans,level+1);
+        Lview(root->right,ans,level+1);
+    }
+
+
+    vector<int> leftView(Node *root) {
+        // Your code here
+        vector<int>ans;
+        Lview(root,ans,0);
+
+        return ans;
+    }
+};
