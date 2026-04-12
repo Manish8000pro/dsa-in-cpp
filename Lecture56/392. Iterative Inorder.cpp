876efb980998f82c480fbe696610a47b3d465080
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-12 21:36:46 +0530

    sol of 392

--- Lecture56/392. Iterative Inorder.cpp
+++ Lecture56/392. Iterative Inorder.cpp
@@ -1,2 +1,52 @@
 // 392. Iterative Inorder
 
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> inOrder(Node* root) {
+        // code here
+        vector<int>ans;
+        Node* curr = root;
+
+        while(curr){
+            // Left doesent exist
+            if(!curr->left){
+                ans.push_back(curr->data);
+                curr = curr->right;
+            }
+            else{
+                Node* pred = curr->left;
+                // Move to the right most node
+
+                while(pred->right&&pred->right!= curr){
+                    pred = pred->right;
+                }
+
+                if(!pred->right){
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    pred->right = NULL;
+                    ans.push_back(curr->data);
+                    curr = curr->right;
+                }
+            } 
+        }
+        return ans;
+    }
+    
+};
