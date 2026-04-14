34b84c28b0929a432a443d9351aa8197f4a1c5f6
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-14 19:46:24 +0530

    search a node in BST

--- Lecture57/412. Search a node in BST.cpp
+++ Lecture57/412. Search a node in BST.cpp
@@ -1,1 +1,40 @@
 // 412. Search a node in BST
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    // Function to search a node in BST.
+    bool search(Node* root, int x) {
+        // Your code here
+
+        // Base condition 
+        if(!root){
+            return 0;
+        }
+
+        if(root->data == x){
+            return 1;
+        }
+
+        if(x<root->data){
+            return search(root->left,x);
+        }
+        else{
+            return search(root->right,x);
+        }
+
+    }
+    
+};
