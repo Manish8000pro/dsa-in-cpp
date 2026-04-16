b2e496d23e5f6092ff7712c0b2172ded03a15db9
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-16 16:00:08 +0530

    Problem 425 solved

---
+++ Lecture57/425. Kth Smallest Element In Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct Node {
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node(int x) : data(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    void inorder(Node*root,vector<int>&ans){
+        if(!root){
+            return;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+
+    int kthSmallest(Node* root, int k) {
+        // Your code here
+        vector<int>ans;
+        inorder(root,ans);
+
+        return ans[k-1];
+    }
+};
