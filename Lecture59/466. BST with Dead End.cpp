5f1ac072d24ae605340add6d83a046b17ee413bb
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-20 17:01:59 +0530

    solution of Bst dead end

--- Lecture59/466. BST with Dead End.cpp
+++ Lecture59/466. BST with Dead End.cpp
@@ -1,1 +1,38 @@
 // check the dead end 
+/**
+ * Definition for a binary tree node.
+ * struct Node {
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node(int x) : data(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    bool checkDeadEnd(Node* root,int minValue , int maxValue){
+        if(!root){
+            return false;
+        }
+        // leaf node 
+        if(!root->left&&!root->right){
+            // if leaf node is dead end 
+            if(minValue==root->data-1 && maxValue==root->data+1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+
+        return checkDeadEnd(root->left,minValue,root->data) || checkDeadEnd(root->right,root->data,maxValue);
+    }
+
+    bool isDeadEnd(Node *root) {
+        // Your code here
+        return checkDeadEnd(root,0,100001);
+    }
+};
