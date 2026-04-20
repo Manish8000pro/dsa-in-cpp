5378c74f4001b43dcf8975e4e633bfebfc2ea6f6
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-20 17:39:12 +0530

    code of  Fixing Two NOdes of a BST

--- Lecture59/475. Fixing Two nodes of a BST.cpp
+++ Lecture59/475. Fixing Two nodes of a BST.cpp
@@ -1,1 +1,48 @@
 // Q.no 475
+/**
+ * Definition for a binary tree node.
+ * struct Node {
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node(int x) : data(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    void inorder(Node *root,Node* &prev,Node* &first,Node* &last){
+        if(!root){
+            return;
+        }
+        inorder(root->left,prev,first,last);
+
+        if(prev && (root->data<prev->data)){
+            // first decrease
+            if(!first){
+                first = prev;
+                last = root;
+            }
+            else{
+                last = root;
+            }
+            // second decrease 
+        }
+        prev = root;
+
+        inorder(root->right,prev,first,last);
+    }
+
+
+    void correctBST(struct Node *root) {
+        // Your code here
+        Node* prev = NULL,*first = NULL,*last = NULL;
+
+        inorder(root,prev,first,last);
+
+        if(first){
+            swap(first->data,last->data);
+        }
+    }
+};
