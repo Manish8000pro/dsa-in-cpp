47407cdb0625eb06ebdca88fd4baa78d32758ea5
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-20 14:06:27 +0530

    solution of Problem

--- Lecture59/453. Preorder Traversal and BST.cpp
+++ Lecture59/453. Preorder Traversal and BST.cpp
@@ -1,1 +1,24 @@
 // Pre order traversal and BST
+
+class Solution {
+public:
+
+    void buildBST(int arr[],int N,int minValue, int maxValue,int &index){
+        if(index>=N || arr[index]<minValue || arr[index]>maxValue){
+            return;
+        }
+
+        // Node build kr sakte ho 
+        int val = arr[index];
+        index++;
+        buildBST(arr,N,minValue,val,index);
+        buildBST(arr,N,val,maxValue,index);
+    }
+
+    int canRepresentBST(int arr[], int N) {
+        // Your code here
+        int index = 0;
+        buildBST(arr,N,INT_MIN,INT_MAX,index);
+        return index>=N;
+    }
+};
