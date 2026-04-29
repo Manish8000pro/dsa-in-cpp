db4c9759808d327d27c6a3292d8d61569ae7000e
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-29 12:27:07 +0530

    left largest

--- Lecture62/Heapcreate.cpp
+++ Lecture62/Heapcreate.cpp
@@ -5,6 +5,11 @@ void heapify(int arr[],int index,int N){
     int largest = index;
     int left = 2*index+1;
     int right = 2*index+2;
+
+    if(left<N && arr[left]>arr[largest])
+    largest = left;
+
+    if()
 }
 
 int main(){
