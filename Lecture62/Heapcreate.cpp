f234f87bf84dc36623ad7268bbf15f422f1b4336
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-29 12:24:52 +0530

    creating left right node

--- Lecture62/Heapcreate.cpp
+++ Lecture62/Heapcreate.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 
+void heapify(int arr[],int index,int N){
+    int largest = index;
+    int left = 2*index+1;
+    int right = 2*index+2;
+}
+
 int main(){
-    
+
 }
