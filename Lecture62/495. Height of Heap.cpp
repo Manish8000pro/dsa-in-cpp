ef1cd86ce2262fe97f39304dee45eea90b0253c4
Manish8000pro <manishkumawat6227@gmail.com>
2026-04-30 12:20:23 +0530

    solution of Height of Heap

--- Lecture62/495. Height of Heap.cpp
+++ Lecture62/495. Height of Heap.cpp
@@ -1,1 +1,18 @@
 // 495. Height of Heap
+class Solution {
+public:
+    int heapHeight(int N, int arr[]) {
+        // Your code here
+        if(N==1){
+            return 1;
+        }
+
+        int height = 0;
+
+        while(N>1){
+            N/=2;
+            height++;
+        }
+        return height;
+    }
+};
