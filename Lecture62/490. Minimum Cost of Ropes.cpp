53dc66f2c28246303b70601e3be0d06cae2d6e81
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-02 20:57:44 +0530

    sol of Q. 490

--- Lecture62/490. Minimum Cost of Ropes.cpp
+++ Lecture62/490. Minimum Cost of Ropes.cpp
@@ -1,1 +1,24 @@
 // 490. Minimum Cost of Ropes
+class Solution {
+public:
+    long long minCost(vector<long long>& arr) {
+        // code here
+        priority_queue<long long,vector<long long>,greater<long long>> pq(arr.begin(),arr.end());
+
+        long long cost = 0;
+
+        while(pq.size()>1){
+            // first minimum element 
+            int first = pq.top();
+            pq.pop();
+
+            int second = pq.top();
+            pq.pop();
+
+            cost+= first+second;
+            pq.push(first+second);
+        }
+        return cost;
+        
+    }
+};
