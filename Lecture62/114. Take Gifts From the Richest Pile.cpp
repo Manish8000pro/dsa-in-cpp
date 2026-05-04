0cbf06dead7f9616b3168e7ac87fbb8b4fc4a03f
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-04 15:59:44 +0530

    solution of problem

--- Lecture62/114. Take Gifts From the Richest Pile.cpp
+++ Lecture62/114. Take Gifts From the Richest Pile.cpp
@@ -1,1 +1,19 @@
 // Q.114 Takes gifts 
+long long pickGifts(vector<int>& gifts, int k) {
+    // Max heap build kro 
+    priority_queue<int>pq(gifts.begin(),gifts.end());
+
+    while(k--) {
+        int x = pq.top();
+        pq.pop();
+        pq.push((int)sqrt(x));
+    }
+
+    long long answer = 0;
+
+    while(!pq.empty()) {
+        answer += pq.top();
+        pq.pop();
+    }
+    return answer;
+}
