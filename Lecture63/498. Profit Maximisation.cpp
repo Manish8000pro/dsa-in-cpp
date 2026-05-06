42cc4bea4ea05ad454b06a7552d0486db8590bc7
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-06 15:38:54 +0530

    solution of this Question

--- Lecture63/498. Profit Maximisation.cpp
+++ Lecture63/498. Profit Maximisation.cpp
@@ -1,1 +1,22 @@
 // 498. Profit Maximisation
+class Solution {
+public:
+    int solve(vector<int>& A, int B) {
+        // Your code here
+        priority_queue<int>pq(A.begin(),A.end());
+
+        int profit = 0;
+
+        while(B&&(pq.size())){
+            int ticketPrice = pq.top();
+            pq.pop();
+            profit+=ticketPrice;
+
+            if(ticketPrice>1){
+                pq.push(ticketPrice-1);
+            }
+            B--;
+        }
+        return profit;
+    }
+};
