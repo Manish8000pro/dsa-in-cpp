5a730097b59636cb37858af64882bf89fd1dd322
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-06 16:25:23 +0530

    solution of 118

--- Lecture64/118. Kth Largest in a Stream.cpp
+++ Lecture64/118. Kth Largest in a Stream.cpp
@@ -1,1 +1,21 @@
 // 118. Kth Largest in a Stream
+vector<int> kthLargest(vector<int>& arr, int k) {
+    // min heap 
+    priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.begin()+k);
+
+    // k = 4 , first 3 answer  = -1;
+
+    vector<int>ans(k-1,-1);
+    ans.push_back(pq.top());
+
+    for(int i=k;i<arr.size();i++){
+        // when we have to pop from the priority queue , top k largest  element 
+
+        if(pq.top()<arr[i]){
+            pq.pop();
+            pq.push(arr[i]);
+        }
+        ans.push_back(pq.top());
+    }
+    return ans;
+}
