65451dac654f0ac1442d2966b5aa6467c7251101
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-06 16:00:09 +0530

    answer of this question

--- Lecture63/364. Kth Smallest Element.cpp
+++ Lecture63/364. Kth Smallest Element.cpp
@@ -1,1 +1,18 @@
 // 364. Kth Smallest Element
+int kthSmallest(vector<int> &arr, int k) {
+    // Write your code here
+    priority_queue<int>pq;
+
+    for(int i = 0;i<k;i++){
+        pq.push(arr[i]);
+    }
+
+    for(int i=k;i<arr.size();i++){
+        if(pq.top()>arr[i]){
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+
+    return pq.top();
+}
