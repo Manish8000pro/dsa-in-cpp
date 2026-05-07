4899184fcccbfc082c8a86d552013c9d6495ab06
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-07 21:32:03 +0530

    How to find element in set

--- Lecture65/set.cpp
+++ Lecture65/set.cpp
@@ -14,20 +14,28 @@ int main(){
     //     cout<<x<<" ";
     // }
 
-    set<pair<int,int>>s1;
-    s1.insert({10,20});
-    s1.insert({5,11});
-    s1.insert({54,8});
-    s1.insert({7,9});
-    s1.insert({10,11});
+    // set<pair<int,int>>s1;
+    // s1.insert({10,20});
+    // s1.insert({5,11});
+    // s1.insert({54,8});
+    // s1.insert({7,9});
+    // s1.insert({10,11});
 
     // for(auto x: s1){
     //     cout<<x.first<<" "<<x.second<<endl;
     // }
 
     // cout<<s1.size();
-    s1.erase(10,11);
-    cout<<s1.size();
+    // s1.erase({10,11});
+    // cout<<s1.size();
+
+    set<int>s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
+
+    // auto it = s.find(90);
+    // cout<<*it<<endl;
 
     return 0;
 }
