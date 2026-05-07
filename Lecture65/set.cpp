d286d91743a08898622ec4339b678c7b6ca08fdc
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-07 21:25:03 +0530

    inserting

---
+++ Lecture65/set.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main(){
+    // set<int>s;
+    // s.insert(10);
+    // s.insert(20);
+    // s.insert(30);
+    // s.insert(40);
+    // s.insert(50);
+
+    // for(auto x: s){
+    //     cout<<x<<" ";
+    // }
+
+    set<pair<int,int>>s1;
+    s1.insert({10,20});
+    s1.insert({5,11});
+    s1.insert({54,8});
+    s1.insert({7,9});
+    s1.insert({10,20});
+
+    for(auto x: s1){
+        cout<<x.first<<" "<<x.second<<endl;
+    }
+
+    return 0;
+}
