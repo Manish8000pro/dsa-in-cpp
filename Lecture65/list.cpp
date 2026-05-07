6f21eccd4ba0af4cfa6d5a872451f6d4f8998f30
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-07 19:06:21 +0530

    adding list element

--- Lecture65/list.cpp
+++ Lecture65/list.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(){
-    
+    list<int>l1;
+    l1.push_back(10);
+    l1.push_back(30);
+    l1.push_back(70);
+    l1.push_front(13);
+    l1.push_front(8);
+
+    for(auto x: l1){
+        cout<<x<<" ";
+    }
 }
