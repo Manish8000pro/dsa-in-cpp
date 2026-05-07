012b96ca8b6053f3f654f499d96b6b9ca44fee62
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-07 16:47:11 +0530

    making pairs

--- Lecture65/pairs.cpp
+++ Lecture65/pairs.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    pars<int,int>p;
+    // pair<int,int>p;
+
+    // p = make_pair(20,40);
+
+    // cout<<p.first<<" "<<p.second<<" ";
+
+    pair<string,int>p;
+
+    p.first = "Manish";
+    p.second = 30;
+
+    cout<<p.first<<" "<<p.second<<" ";
+
+    return 0;
 }
