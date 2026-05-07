a381b126dd36a58a93bc0544c33af5eac154e053
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-07 17:02:31 +0530

    printing

--- Lecture65/pairs.cpp
+++ Lecture65/pairs.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -18,12 +19,22 @@ int main(){
     // 2 value 
     // 3 value ke pair bnane ho to 
 
-    pair<string,pair<int,int>>p;
-    p.first = "Manish";
-    p.second.first = 30;
-    p.second.second = 40;
+    // pair<string,pair<int,int>>p;
+    // p.first = "Manish";
+    // p.second.first = 30;
+    // p.second.second = 40;
 
-    cout<<p.first<<" "<<p.second.first<<" "<<p.second.second<<" ";
+    // cout<<p.first<<" "<<p.second.first<<" "<<p.second.second<<" ";
+
+    vector<int>a = {10,20,30,40,50};
+
+    // for(int i=0;i<a.size();i++){
+    //     cout<<a[i]<<" ";
+    // }
+
+    for(int x : a){
+        cout<<x<<" ";
+    }
 
     return 0;
 }
