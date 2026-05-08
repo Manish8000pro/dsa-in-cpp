f2d0592d530e710769e3b88f5c0fede6ab013c03
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-08 11:46:02 +0530

    unordered map

---
+++ Lecture66/unordered.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    map<string,int>m;
+
+    // how to insert value inside it 
+    m["Manish"] = 10;
+    m["Dinesh"] = 20;
+    m["Vikash"] = 30;
+
+    // cout<<m["Manish"]<<" ";
+
+    // second method
+    m.insert(make_pair("Ram",50));
+    // cout<<m["Ram"]<<" ";
+
+    for(auto x: m){
+        cout<<x.first<<" "<<x.second<<endl;
+    }
+
+    return 0;
+}
