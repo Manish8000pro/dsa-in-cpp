3ea94a30ef2257bf965a3da06958523dc302e0b0
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-08 11:55:08 +0530

    Finding element in map

--- Lecture66/unordered.cpp
+++ Lecture66/unordered.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<map>
+#include<unordered_map>
+// #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    map<string,int>m;
+    unordered_map<string,int>m;
 
     // how to insert value inside it 
     m["Manish"] = 10;
@@ -16,8 +18,16 @@ int main(){
     m.insert(make_pair("Ram",50));
     // cout<<m["Ram"]<<" ";
 
-    for(auto x: m){
-        cout<<x.first<<" "<<x.second<<endl;
+    // for(auto x: m){
+    //     cout<<x.first<<" "<<x.second<<endl;
+    // }
+
+    auto it = m.find("Manish");
+    if(it!=m.end()){
+        cout<<"Found";
+    }
+    else{
+        cout<<"Not Found";
     }
 
     return 0;
