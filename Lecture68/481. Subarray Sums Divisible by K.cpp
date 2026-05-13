8ec3a054634e6dcc4c034e21f6a2daff5f35a0d9
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-13 13:34:05 +0530

    Sol. of Question

--- Lecture68/481. Subarray Sums Divisible by K.cpp
+++ Lecture68/481. Subarray Sums Divisible by K.cpp
@@ -1,1 +1,20 @@
 // 481. Subarray Sums Divisible by K
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // Code here
+        unordered_map<int,int>count;
+        count[0] = 1;
+
+        int prefixSum = 0, result = 0;
+        int rem;
+
+        for(int value: nums){
+            prefixSum+=value;
+            rem = ((prefixSum%k)+k)%k;
+            result+=count[rem];
+            count[rem]++;
+        }
+        return result;
+    }
+};
