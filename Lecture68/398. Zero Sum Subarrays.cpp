1e06ce2322c65df9f844598705b2ec4c2ababf74
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-13 13:17:34 +0530

    Solution of Q.

--- Lecture68/398. Zero Sum Subarrays.cpp
+++ Lecture68/398. Zero Sum Subarrays.cpp
@@ -1,1 +1,24 @@
 // 398. Zero Sum Subarrays
+class Solution {
+public:
+    // Function to count subarrays with sum equal to 0.
+    long long findSubarray(vector<long long> &arr, int n) {
+        // Your code here
+        long long prefixSum = 0;
+        unordered_map<long long,int>count;
+        long long result = 0;
+
+        count[0] = 1;
+
+        for(long long val: arr){
+            prefixSum+=val;
+
+            // if prefixSum exist in Unordered map , it will return teh count of prefix sum 
+            // else if will return 0;
+
+            result += count[prefixSum];
+            count[prefixSum]++;
+        }
+        return result;
+    }
+};
