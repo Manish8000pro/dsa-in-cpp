ae40b56ca7913c27b16fd5269f0d2899fc169294
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-15 16:26:00 +0530

    solution of 7 Q.

--- Lecture69/7. Minimum Size Subarray Sum.cpp
+++ Lecture69/7. Minimum Size Subarray Sum.cpp
@@ -1,1 +1,17 @@
 // 7. Minimum Size Subarray Sum 
+int minSubArrayLen(int target, const vector<int>& nums) {
+    int minWindowSize = nums.size()+1,n = nums.size(),left = 0,right = 0,sum = 0;
+
+    while(right<n){
+        sum+= nums[right];
+        
+        while(sum>=target){
+            minWindowSize = min(minWindowSize,right-left+1);
+            sum-=nums[left];
+            left++;
+        }
+        right++;
+        
+    }
+    return minWindowSize == n+1 ? 0: minWindowSize;
+}
