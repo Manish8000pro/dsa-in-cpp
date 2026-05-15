3048b9dda18e64b228458bdf6897f2f7124b6d4a
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-15 16:09:44 +0530

    Solution of 122

--- Lecture69/122. Count Subarrays With Score Less Than K.cpp
+++ Lecture69/122. Count Subarrays With Score Less Than K.cpp
@@ -1,1 +1,22 @@
 // 122. Count Subarrays With Score Less Than K
+long long countSubarrays(vector<int>& nums, long long k) {
+    long long count = 0 ,sum=0,product;
+    int left = 0 , right = 0,n = nums.size();
+
+    while(right<n){
+        sum+=nums[right];
+
+        // multiplication ; sum*(right-left+1);
+        product = sum*(right-left+1);
+
+        while(product>=k){
+            sum-=nums[left];
+            left++;
+            product = sum*(right-left+1);
+        }
+        
+        count+=(right-left+1);
+        right++;
+    }
+    return count;
+}
