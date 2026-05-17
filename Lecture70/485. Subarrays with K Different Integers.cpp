366f8a47f678f13bcbd620488813b90457956b3c
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-17 17:39:50 +0530

    Q. solved

--- Lecture70/485. Subarrays with K Different Integers.cpp
+++ Lecture70/485. Subarrays with K Different Integers.cpp
@@ -1,2 +1,31 @@
  // 485. Subarrays with K Different Integers
- 
+class Solution {
+public:
+    int atleast(vector<int>&nums,int k){
+            unordered_map<int,int>count;
+            int left = 0,right = 0,n=nums.size(),result = 0;
+
+            while(right<n){
+                count[nums[right]]++;
+
+                if(count[nums[right]]==1){
+                    k--;
+                }
+                while(k==0){
+                    result+=(n-right);
+                    count[nums[left]]--;
+
+                    if(count[nums[left]]==0){
+                        k++;
+                    }
+                    left++;
+                }
+                right++;
+            }
+            return result;
+        }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        // Code here
+        return atleast(nums,k) - atleast(nums,k+1);
+    }
+};
