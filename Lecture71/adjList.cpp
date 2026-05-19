55b68475a98495c2dc6d8f9eaddae9fa75ffed8e
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-19 19:56:36 +0530

    adj List making in graph

---
+++ Lecture71/adjList.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    // tell me number vertex(node) edges in your graph 
+    int vertex,edges;
+    cout<<"Enter the vertex and edges-> ";
+    cin>>vertex>>edges;
+    // create the adjancy List
+    vector<vector<int>>adjList(vertex);
+    
+    // Enter the edges
+    int u,v;
+
+    for(int i=0,i<edges:i++){
+        cin>>u>>v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    };
+
+    // Print the element of graph
+    for(int i = 0;i<vertex;i++){
+        cout<<" edges-> ";
+        for(int j=0;j<adjList[i].size();j++){
+            cout<<adjList[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    
+    return 0;
+}
