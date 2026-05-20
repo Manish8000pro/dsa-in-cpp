1ecbe75409f92329007d85e8ef1498a57eaa8d67
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-20 21:57:16 +0530

    adj list weighted

--- Lecture71/adjList weighted.cpp
+++ Lecture71/adjList weighted.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main(){
-    // tell me number vertex(node) edges in your graph 
-    int vertex,edges;
-    cout<<"Enter the vertex and edges-> ";
+   
+    // tell me number of vertex(node) and edges in your graph
+    int vertex, edges;
+    cout<<"Enter vertex and edges-> ";
     cin>>vertex>>edges;
-    // create the adjancy List
+
+    // Create Adjacency List
     vector<vector<pair<int,int>>>adjList(vertex);
-    
-    // Enter the edges
-    int u,v weight;
+
+    // Enter the edges 
+    int u,v, weight;
 
     for(int i=0;i<edges;i++){
         cin>>u>>v>>weight;
@@ -19,15 +21,18 @@ int main(){
         adjList[v].push_back({u,weight});
     };
 
-    // Print the element of graph
-    for(int i = 0;i<vertex;i++){
-        cout<<" edges-> ";
+
+    //  print the element of graph
+
+    for(int i=0;i<vertex;i++){
+        cout<<i<<" edges-> ";
         for(int j=0;j<adjList[i].size();j++){
-            cout<<adjList[i][j].first<<" "<<adjList[i][j].second;
+            cout<<adjList[i][j].first<<" "<<adjList[i][j].second<<" ";
         }
         cout<<endl;
     }
-
     
+
     return 0;
+
 }
