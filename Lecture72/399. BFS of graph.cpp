4ec7e64eed09ec9591d1fd2e76afa6bf8d7790de
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-20 21:43:12 +0530

    Q. 399 solved

--- Lecture72/399. BFS of graph.cpp
+++ Lecture72/399. BFS of graph.cpp
@@ -1,2 +1,42 @@
  // 399. BFS of graph
- 
+ class Solution {
+public:
+
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        
+        queue<int>q;
+        vector<bool>visited(V,0);
+
+        // push 0 into queue and mark its visit to 1
+        q.push(0);
+        visited[0] = 1;
+
+        vector<int>result;
+
+        while(!q.empty()){
+            int node = q.front();
+            result.push_back(node);
+            q.pop();
+
+            // Look at all the neighbour of this node
+            // for(int i=0;i<adj[node].size();i++){
+            //     int neighbour = adj[node][i];
+
+            //     if(!visited[neighbour]){
+            //         visited[neighbour] =1;
+            //         q.push(neighbour);
+            //     }
+            // }
+
+            for(int neighbour: adj[node]){
+                if(!visited[neighbour]){
+                    visited[neighbour] =1;
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return result;
+    }
+
+};
