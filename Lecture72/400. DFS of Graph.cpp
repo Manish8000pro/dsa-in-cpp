4cf251d07612b1016ca8c415ed1a0943fbdffb9c
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-21 14:53:37 +0530

    Q. 400 solved

--- Lecture72/400. DFS of Graph.cpp
+++ Lecture72/400. DFS of Graph.cpp
@@ -1,1 +1,21 @@
 // 400. DFS of Graph
+// Function to return a list containing the DFS traversal of the graph.
+void DFS(vector<int>adj[],int node,vector<bool>&visited,vector<int>&result){
+    visited[node]=1;
+    result.push_back(node);
+
+    for(int neib: adj[node]){
+        if(!visited[neib]){
+            DFS(adj,neib,visited,result);
+        }
+    }
+}
+
+vector<int> dfsOfGraph(int v, vector<int> adj[]) {
+    vector<int>result;
+    vector<bool>visited(v,0);
+
+    DFS(adj,0,visited,result);
+
+    return result;
+}
