1b703badfd2e8be6e6e287a7c25d5ee03bb7e3e2
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-24 17:49:49 +0530

    proper solution of Problem

--- Lecture74/193. Topological Sort.cpp
+++ Lecture74/193. Topological Sort.cpp
@@ -1,2 +1,38 @@
  // 193. Topological Sort
- 
+ class Solution {
+public:
+    
+    void topSort(vector<int>adj[],int node,vector<bool>&visited,vector<int>&result){
+        visited[node]=1;
+
+        for(int neib: adj[node]){
+            if(!visited[neib]){
+                topSort(adj,neib,visited,result);
+            }
+        }
+
+        result.push_back(node);
+    }
+
+    vector<int> topologicalSort(int v, vector<vector<int>>& edges) {
+        // Your code here
+        vector<int>adj[v];
+
+        for(auto edge: edges){
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        vector<bool>visited(v,0);
+        vector<int>result;
+
+        for(int i=0;i<v;i++){
+            if(!visited[i]){
+                topSort(adj,i,visited,result);
+            }
+        }
+
+        reverse(result.begin(),result.end());
+
+        return result;
+    }
+};
