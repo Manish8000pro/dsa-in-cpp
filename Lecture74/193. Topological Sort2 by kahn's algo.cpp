af0fc8ea8ebb2db1c1a861baf0a331e638cdd03a
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-25 18:30:45 +0530

    solution by kahn's algorithm

--- Lecture74/193. Topological Sort2 by kahn's algo.cpp
+++ Lecture74/193. Topological Sort2 by kahn's algo.cpp
@@ -1,1 +1,48 @@
- // 193. Topological Sort by Kahn's algorithm
+// 193. Topological Sort by Kahn's algorithm
+class Solution {
+public:
+    vector<int> topologicalSort(int V, vector<vector<int>>& edges) {
+        // Your code here
+        // kahn's algorithm
+        vector<int>adj[V];
+        vector<int>inDegree(V,0);
+
+        // find the inDegree of each node
+
+        for(auto edge: edges){
+            // u --> v
+            // edge[0]-->edge[1]
+
+            int u = edge[0];
+            int V = edge[1];
+            adj[u].push_back(V);
+            inDegree[V]++;
+        }
+        queue<int>q;
+
+        // put all thos node into the queue whose inDegree is Zero
+
+        for(int i=0;i<V;i++){
+            if(!inDegree[i]){
+                q.push(i);
+            }
+        }
+
+        vector<int>result;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+
+            for(int neib: adj[node]){
+                inDegree[neib]--;
+                if(!inDegree[neib]){
+                    q.push(neib);
+                }
+            }
+        }
+
+        return result;
+    }
+};
