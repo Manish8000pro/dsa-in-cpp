9782c42534530c901056ed5aec7f3a1edbc87d69
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-28 18:25:08 +0530

    created the demo.

--- Lecture77/Demo.cpp
+++ Lecture77/Demo.cpp
@@ -9,21 +9,27 @@ class student{
     public:
     string name;
     int age;
-    int rollnumber
+    int rollnumber;
     string college;
 
-    student(string n, int a, int rn,string c){
-        name = n;
-        age = a;
-        rollnumber = rn;
-        college = c;
-    }
-}
+    // student(string n, int a, int rn,string c){
+    //     name = n;
+    //     age = a;
+    //     rollnumber = rn;
+    //     college = c;
+    // }
+};
 
 int main(){
 
     // stack & Heap
     student s1;
+    s1.name = "Manish";
+    s1.age = 20;
+    s1.rollnumber = 7;
+    s1.college = "IIT Guhawati";
+
+    cout<<s1.name<<" "<<s1.age<<" "<<s1.rollnumber<<" "<<s1.college<<" ";
 
     return 0;
 }
---
+++ Lecture77/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+student(string n, int a, int rn,string c){
+    //     name = n;
+    //     age = a;
+    //     rollnumber = rn;
+    //     college = c;
+    // }
