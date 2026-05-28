3a6c6bcc4d000c23c0eaa0614d6b70727610629c
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-28 18:17:34 +0530

    crating object in oops

---
+++ Lecture77/Demo.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+// class and object
+
+class student{
+
+    public:
+    string name;
+    int age;
+    int rollnumber
+    string college;
+
+    student(string n, int a, int rn,string c){
+        name = n;
+        age = a;
+        rollnumber = rn;
+        college = c;
+    }
+}
+
+int main(){
+
+    // stack & Heap
+    student s1;
+
+    return 0;
+}
