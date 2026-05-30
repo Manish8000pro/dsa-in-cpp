611ce5711544af4ff6cdd3960d89c307b3d21285
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-30 15:20:14 +0530

    solved pro. no. 199

--- Lecture77/199. Find The Number Of Islands.cpp
+++ Lecture77/199. Find The Number Of Islands.cpp
@@ -1,1 +1,40 @@
 // 199. Find The Number Of Islands
+bool isValid(int i , int j, int r,int c){
+    return i>=0 && i<r && j>=0 && j<c;
+}
+
+void DFS(vector<vector<char>>&grid,int i,int j){
+    grid[i][j]='W';
+
+    int row[8] = {0,0,1,1,1,-1,-1,-1};
+    int col[8] = {1,-1,0,1,-1,0,1,-1};
+
+    int r = grid.size();
+    int c = grid[0].size();
+
+    // I can go into 8 directions , follow the direction which has land 
+
+
+    for(int k = 0;k<8;k++){
+        if(isValid(i+row[k],j+col[k],r,c) && grid[i+row[k]][j+col[k]]=='L'){
+           DFS(grid,i+row[k],j+col[k]);
+        }
+    }
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int totalNumberOfIsland = 0;
+
+    int r = grid.size();
+    int c = grid[0].size();
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(grid[i][j]=='L'){
+                DFS(grid,i,j);
+                totalNumberOfIsland++;
+            }
+        }
+    }
+    return totalNumberOfIsland;
+}
