418b1968f63d09bf10432e5e2d764a242ae97fd0
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-01 17:50:00 +0530

    constructer

--- Lecture78/Inheritance.cpp
+++ Lecture78/Inheritance.cpp
@@ -10,13 +10,29 @@ class Student {
     int rollNumber;
     string college;
 
+     public:
+    Student() {}
+    
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
     void markAttendance(){
         cout<<name<< " is present"<<endl;
     }
 };
 
 class EngineeringStudent: public Student {
-   
+   public:
+   EngineeringStudent(string n, int a){
+        name = n;
+        age = a;
+   }
+
+   void fun(){
+        cout<<name<< " "<< age << endl;
+   }
 };
 
 // DRY - Do not repeat youself
@@ -26,14 +42,15 @@ int main(){
 
     EngineeringStudent *e1 = new EngineeringStudent();
 
-    s1->name = "Manish";
-    s1->age = 21;
+    // s1->name = "Manish";
+    // s1->age = 21;
 
-    e1->name = "Dinesh";
-    e1->age = 19;
+    // e1->name = "Dinesh";
+    // e1->age = 19;
 
-    s1->markAttendance();
-    e1->markAttendance();
+    // s1->markAttendance();
+    // e1->markAttendance();
+    e1->fun();
 
     return 0;
 }
