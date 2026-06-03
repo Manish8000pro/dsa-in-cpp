351c32271f0141ee389217c90e9260a4c4700c2b
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-03 19:17:03 +0530

    error solving

--- Lecture78/singleInheritance.cpp
+++ Lecture78/singleInheritance.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 class Student {
      public:
-     string n;
+     string name;
      int a;
 
      void markAttendance(){
-        cout<<n<< " is present"<<endl;
+        cout<<name<< " is present"<<endl;
     }
 
 };
 
 class EngineeringStudent: public Student{
      public:
-     string n;
+     string name;
      int a;
 
      void attendentLab(){
-          cout<<" Lab attendent by "<<n<<endl;
+          cout<<" Lab attendent by "<<name<<endl;
      }
 };
 
@@ -27,7 +27,7 @@ int main(){
      // single  inheritance
      EngineeringStudent *es1 = new EngineeringStudent();
 
-     es1->n = "Manish";
+     es1->name = "Manish";
      es1->a = 20;
 
      es1->markAttendance();
