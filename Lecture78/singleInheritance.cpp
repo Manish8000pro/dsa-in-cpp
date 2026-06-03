460b4d6b6e2dce545100d1c6bfd27a3984690341
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-03 19:15:08 +0530

    single in heritance making

--- Lecture78/singleInheritance.cpp
+++ Lecture78/singleInheritance.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+class Student {
+     public:
+     string n;
+     int a;
+
+     void markAttendance(){
+        cout<<n         << " is present"<<endl;
+    }
+
+};
+
+class EngineeringStudent: public Student{
+     public:
+     string n;
+     int a;
+
+     void attendentLab(){
+          cout<<" Lab attendent by "<<n<<endl;
+     }
+};
+
 int main(){
      // single  inheritance
+     EngineeringStudent *es1 = new EngineeringStudent();
+
+     es1->n = "Manish";
+     es1->a = 20;
+
+     es1->markAttendance();
+     es1->attendentLab();
+
+     return 0;
+
 }
