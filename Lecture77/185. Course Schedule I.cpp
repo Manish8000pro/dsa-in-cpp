aa1bf6fa6a351e540fa513b1ba9b2e992faae713
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-03 12:34:31 +0530

    solution of 185

--- Lecture77/185. Course Schedule I.cpp
+++ Lecture77/185. Course Schedule I.cpp
@@ -1,1 +1,60 @@
- // 185. Course Schedule I
+ // 185. Course Schedule II
+ class Solution {
+public:
+
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+
+        for(auto edge : prerequisites){
+
+            int u = edge[1];
+            int v = edge[0];
+
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+
+        queue<int> q;
+
+        for(int i=0;i<n;i++){
+
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+
+        vector<int> result;
+
+
+        while(!q.empty()){
+
+            int node = q.front();
+            q.pop();
+
+            result.push_back(node);
+
+
+            for(int neighbour : adj[node]){
+
+                indegree[neighbour]--;
+
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+
+
+        // cycle present
+        if(result.size()!=n){
+            return {};
+        }
+
+
+        return result;
+    }
+};
