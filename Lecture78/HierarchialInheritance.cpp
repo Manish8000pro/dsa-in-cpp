ecaff48776fd4554af14a6ac6dcebd9a90ad7172
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-07 12:08:37 +0530

    Hierarchicla inheritance making

--- Lecture78/HierarchialInheritance.cpp
+++ Lecture78/HierarchialInheritance.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+class Student {
+    public:
+    string name;
+    int age;
 
+
+    void markAttendance(){
+        cout<<name<<" is present"<<endl;
+    } 
+};
+
+class EngineeringStudent: public Student{
+    public:
+
+    void attendLab(){
+        cout<<"Lab attend by the"<<name<<endl;
+    }
+};
+
+class medicalStudent: public Student{
+    public:
+    void performeOperation(){
+        cout<<name<<" is perform the operation"<<endl;
+    }
+};
 
 int main(){
+    EngineeringStudent *es1 = new EngineeringStudent();
+    es1->name = "Manish";
+    es1->age = 20;
 
+    medicalStudent *ms1 = new medicalStudent();
+    ms1->name = "Naresh";
+    ms1->age = 24;
+
+    ms1->markAttendance();
+    ms1->performeOperation();
 }
