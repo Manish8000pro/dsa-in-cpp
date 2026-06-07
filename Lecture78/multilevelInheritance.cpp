be1b0fbb25f7f8d4b1f3008084d7887fbb4926ed
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-07 11:43:49 +0530

    making the multilevel inheritance

---
+++ Lecture78/multilevelInheritance.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+class Student {
+    public:
+    string name;
+    int age;
+
+    void markAttedance() {
+        cout << name << " is present" << endl;
+    }
+};
+
+class EngineeringStudent: public Student{
+     public:
+
+     void attendentLab(){
+          cout<<" Lab attendent by "<<name<<endl;
+     }
+};
+
+class CSEStudent: public EngineeringStudent {
+
+    void printCode(){
+        cout<< name<< " is printing code"<<endl;
+    }
+};
+
+int main(){
+    CSEStudent *cs1 = new CSEStudent();
+
+    cs1->name = "Manish";
+    cs1->age = 20;
+
+    cs1->markAttedance();
+    cs1->attendentLab();
+    cs1->printCode();
+
+    
+}
