4a1ed6ddf06753ecef39fe9521154e9b7de93e7f
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-09 17:50:24 +0530

    ans of Belman-ford

--- Lecture84/428. Bellman-Ford.cpp
+++ Lecture84/428. Bellman-Ford.cpp
@@ -1,1 +1,46 @@
 //  428. Bellman-Ford
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // code here
+        vector<int>dist(V,100000000);
+        dist[src]=0;
+
+        // relax all the edges n-1 time
+
+        for(int i=0;i<V-1;i++){
+            for(auto edge: edges){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+
+                if(dist[u]==100000000)
+                continue;
+
+                if(dist[v]>dist[u]+wt){
+                    dist[v] = dist[u]+wt;
+                }
+            }
+        }
+
+        // 1 last time relax the edges, if dist valye change, answer is not possible
+
+            for(auto edge: edges){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+
+                if(dist[u]==100000000)
+                continue;
+
+                if(dist[v]>dist[u]+wt){
+                    vector<int>ans;
+                    ans.push_back(-1);
+                    return ans;
+                }
+            }
+
+            return dist;
+    }
+
+};
