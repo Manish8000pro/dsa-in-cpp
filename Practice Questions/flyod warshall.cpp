7fcc905beba7c4bec207bf6d721db0068297df3d
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-10 21:47:07 +0530

    solution

--- Practice Questions/flyod warshall.cpp
+++ Practice Questions/flyod warshall.cpp
@@ -1,1 +1,26 @@
 // Flyod warshall 
+void floydWarshall(vector<vector<int>>& dist) {
+    int n = dist.size();
+    for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+    if(dist[i][j]==-1)
+    dist[i][j]=INT_MAX;
+
+    // floyd warshall algorithm
+
+    for(int k=0;k<n;k++)
+    for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+
+    {
+        if(dist[i][k]==INT_MAX || dist[k][j]==INT_MAX)
+        continue;
+        dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+    }
+
+    for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+    
+    if(dist[i][j]==INT_MAX)
+    dist[i][j]=-1;
+}
