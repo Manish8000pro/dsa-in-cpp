f91fab0db4f843af587413ac0bec051290a22151
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-12 16:00:36 +0530

    ans of floyd warshall

--- Lecture85/196. Floyd Warshall.cpp
+++ Lecture85/196. Floyd Warshall.cpp
@@ -1,1 +1,17 @@
 // 196. Floyd Warshall
+void floydWarshall(vector<vector<int>>& dist) {
+    int V = dist.size();
+
+    for(int k=0;k<V;k++){
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(dist[i][k]==100000000 || dist[k][j]==100000000)
+                continue;
+
+                // k== row , k==cols 
+
+                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+        }
+    }
+}
