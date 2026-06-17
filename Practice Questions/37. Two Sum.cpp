817e0436e503989573ce23cfde7152cdd46cb5d5
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-17 17:23:28 +0530

    solution of two sum

--- Practice Questions/37. Two Sum.cpp
+++ Practice Questions/37. Two Sum.cpp
@@ -1,1 +1,17 @@
 // 37. Two Sum
+vector<int> twoSum(vector<int>& nums, int target) {
+    // Your code here
+    unordered_map<int,int> mp;
+
+    for(int i=0;i<nums.size();i++){
+
+        int need = target - nums[i];
+
+        if(mp.count(need))
+            return {mp[need], i};
+
+        mp[nums[i]] = i;
+    }
+
+    
+}
