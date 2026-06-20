f3da79bb3fd996c9e9e0fb3d00876e93755d0c3f
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-20 16:38:23 +0530

    solved problem no. 441

--- Lecture89/441. Hamiltonian Path.cpp
+++ Lecture89/441. Hamiltonian Path.cpp
@@ -1,2 +1,42 @@
 // 441. Hamiltonian Path
 // solution
+class Solution {
+public:
+
+    bool findHamiPath(vector<int>adj[],int node,vector<bool>&visited,int &count,int n){
+        visited[node] = 1;
+        count++;
+        if(count==n)
+        return 1;
+
+        for(auto neib: adj[node]){
+            if(!visited[neib]&&findHamiPath(adj,neib,visited,count,n))
+            return 1;
+        }
+
+        visited[node] = 0;
+        count--;
+
+        return 0;
+    }
+
+    bool check(int n, int m, vector<vector<int>>& edges) {
+        // code here
+        vector<bool>visited(n,0);
+        vector<int>adj[n];
+
+        for(auto edge: edges){
+            adj[edge[0]-1].push_back(edge[1]-1);
+            adj[edge[1]-1].push_back(edge[0]-1);
+        }
+
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            if(findHamiPath(adj,i,visited,count,n))
+            return 1;
+        }
+
+        return 0;
+    }
+};
