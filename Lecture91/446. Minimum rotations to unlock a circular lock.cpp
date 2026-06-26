bd57a9015fb81c789b2853a548e16b6b525bb62f
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-26 16:27:46 +0530

    solution of 446

--- Lecture91/446. Minimum rotations to unlock a circular lock.cpp
+++ Lecture91/446. Minimum rotations to unlock a circular lock.cpp
@@ -1,1 +1,25 @@
 //446. Minimum rotations to unlock a circular lock
+class Solution {
+public:
+    long long rotationCount(long long R, long long D) {
+        // code here
+        long long count = 0;
+
+        while(R){
+            int firstLastValue = R%10;
+            int secondLastValue = D%10;
+
+            R/=10;
+            D/=10;
+
+            count+=min(abs(firstLastValue-secondLastValue),10-abs(firstLastValue-secondLastValue));
+        }
+
+        while(D){
+            int LastValue = D%10;
+            D/=10;
+            count+=min(LastValue,10-LastValue);
+        }
+        return count;
+    }
+};
