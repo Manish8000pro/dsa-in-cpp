51e2edabce7e8c84afa84484cff534a2f92f338b
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-26 16:38:34 +0530

    solution of Q.no. 448

--- Lecture91/448. N meetings in one room.cpp
+++ Lecture91/448. N meetings in one room.cpp
@@ -1,1 +1,26 @@
 //448. N meetings in one room
+class Solution {
+public:
+    int maxMeetings(vector<int>& start, vector<int>& end) {
+        // code here
+        vector<pair<int,int>>meeting;
+        int n = start.size();
+
+        for(int i=0;i<n;i++){
+            meeting.push_back({end[i],start[i]});
+        }
+
+        sort(meeting.begin(),meeting.end());
+
+        int LastMeetingTime = -1;
+        int totalMeeting = 0;
+
+        for(int i=0;i<n;i++){
+            if(meeting[i].second>LastMeetingTime){
+                totalMeeting++;
+                LastMeetingTime = meeting[i].first;
+            }
+        }
+        return totalMeeting;
+    }
+};
