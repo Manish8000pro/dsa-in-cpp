15196a060a2492a3e2b425310df9894809dd0248
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-27 19:04:45 +0530

    solution of 452

--- Lecture92/452. Fractional Knapsack.cpp
+++ Lecture92/452. Fractional Knapsack.cpp
@@ -1,1 +1,44 @@
 //452. Fractional Knapsack
+bool comp(pair<int,int>&a,pair<int,int>&b){
+    return a.first*b.second>=b.first*a.second;
+}
+
+class Solution {
+public:
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        vector<pair<int,int>>arr;
+        int n = val.size();
+
+        for(int i=0;i<n;i++){
+            arr.push_back({val[i],wt[i]});
+        }
+
+        // val/wt
+
+        sort(arr.begin(),arr.end(),comp);
+
+        int i = 0;
+        double totalProfit = 0.0;
+
+        while(i<n&&capacity){
+
+
+            if(arr[i].second<=capacity){
+                totalProfit+= arr[i].first;
+                capacity-=arr[i].second;
+            }
+            else{
+                double a = arr[i].first;
+                a/=arr[i].second;
+
+                totalProfit+= a*capacity;
+                capacity = 0;
+            }
+            i++;
+
+            // capacity ke hisab se select krana padega ,take the fraction of item
+        }
+        return totalProfit;
+    }
+};
