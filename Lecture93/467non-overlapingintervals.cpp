5a4b6c9ad83fb45b518b12d1bc235bc20749d2db
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-28 18:27:20 +0530

    solved problem ,

--- Lecture93/467non-overlapingintervals.cpp
+++ Lecture93/467non-overlapingintervals.cpp
@@ -1,1 +1,25 @@
 //  Non-overlapping Intervals
+bool comp(vector<int>&a,vector<int>&b){
+    return a[1]<=b[1];
+}
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        // code here
+        sort(intervals.begin(),intervals.end(),comp);
+        int lastEndTime = -50000,totalRemoveInterval=0;
+        int n = intervals.size();
+
+
+        for(int i=0;i<n;i++){
+            if(lastEndTime<=intervals[i][0]){
+                lastEndTime = intervals[i][1];
+            }
+            else{
+                totalRemoveInterval++;
+            }
+        }
+        return totalRemoveInterval;
+    }
+};
