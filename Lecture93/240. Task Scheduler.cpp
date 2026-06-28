1850b5a51f7baf18bd929949464eb093ccbafb7c
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-28 18:08:15 +0530

    solution of Task scheduler

--- Lecture93/240. Task Scheduler.cpp
+++ Lecture93/240. Task Scheduler.cpp
@@ -1,1 +1,22 @@
 //240. Task Scheduler
+int leastInterval(vector<char>& tasks, int n) {
+    vector<int>freq(26,0);
+
+    int count = 0;
+
+    for(int i=0;i<tasks.size();i++){
+        int index = tasks[i]-'A';
+        freq[index]++;
+        count = max(count,freq[index]);
+    }
+
+    int result = (n+1)*(count-1);
+
+    // Now whose freq is equal to count increase result by 1;
+
+    for(int i=0;i<26;i++){
+        if(freq[i] == count)
+        result++;
+    }
+    return tasks.size()>result ? tasks.size(): result;
+}
