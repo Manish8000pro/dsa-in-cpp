bb3d63dba4f121a1bbe5cf1e543adb0e9af7b8d9
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-01 13:24:16 +0530

    solved

--- Practice Questions/465. Insert Interval.cpp
+++ Practice Questions/465. Insert Interval.cpp
@@ -1,1 +1,27 @@
 //465. Insert Interval
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // code here
+        vector<vector<int>>ans;
+
+        for(auto interval: intervals){
+            // before interval 
+            if(interval[1]<newInterval[0]){
+                ans.push_back(interval);
+            }
+            else if(interval[0]>newInterval[1]){
+                ans.push_back(newInterval);
+                newInterval = interval;
+            }
+            else{
+                newInterval[0] = min(interval[0],newInterval[0]);
+                newInterval[1] = max(interval[1],newInterval[1]);
+            }
+        }
+        ans.push_back(newInterval);
+        
+        return ans;
+        
+    }
+};
