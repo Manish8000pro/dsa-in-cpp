7fbe7ecefd1f1b7895310a3a5abc3a062137e455
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-02 16:40:28 +0530

    solution of Q.11

--- Practice Questions/11. Push Negatives Back.cpp
+++ Practice Questions/11. Push Negatives Back.cpp
@@ -1,1 +1,18 @@
 //11. Push Negatives Back
+vector<int> moveNegativesToEnd(vector<int>& arr) {
+    // Your code here
+    vector<int>ans;
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]>=0){
+            ans.push_back(arr[i]);
+        }
+    }
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]<0){
+            ans.push_back(arr[i]);
+        }
+    }
+    return ans;
+}
