ca6c7f71b0d867600969573d4f158a18eece98f4
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-03 15:57:01 +0530

    solution of Q. 4

--- Practice Questions/4. Zero Out Matrix.cpp
+++ Practice Questions/4. Zero Out Matrix.cpp
@@ -1,1 +1,54 @@
 // 4. Zero Out Matrix
+void setZeroes(vector<vector<int>>& matrix) {
+
+  int m = matrix.size();
+  int n = matrix[0].size();  
+
+  bool firstRow = false;
+  bool firstCol = false;
+
+  // Check first row
+  for(int j = 0; j < n; j++){
+    if(matrix[0][j] == 0){
+        firstRow = true;
+        break;
+    }
+}
+  // check first col
+  for(int i = 0; i < m; i++){
+    if(matrix[i][0] == 0){
+        firstCol = true;
+        break;
+    }
+}
+  // Mark Row and col
+  for(int i=1;i<m;i++){
+    for(int j=1;j<n;j++){
+        if(matrix[i][j]==0){
+            matrix[i][0]=0;
+            matrix[0][j]=0;
+        }
+    }
+  }
+  // fill Zero
+  for(int i=1;i<m;i++){
+    for(int j=1;j<n;j++){
+        if(matrix[i][0]==0 || matrix[0][j]==0){
+            matrix[i][j]=0;
+        }
+    }
+  }
+  if(firstRow){
+    for(int j=0;j<n;j++){
+        matrix[0][j] = 0;
+    }
+  }
+
+  if(firstCol){
+    for(int i=0;i<m;i++){
+        matrix[i][0] = 0;
+    }
+  }
+
+
+};
