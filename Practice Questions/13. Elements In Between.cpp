4e3934a1352b0cb4414c5cca1179dd79e94e5994
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-04 17:11:25 +0530

    complete solution of Q . 13

--- Practice Questions/13. Elements In Between.cpp
+++ Practice Questions/13. Elements In Between.cpp
@@ -1,1 +1,23 @@
 //13. Elements In Between
+int getCount(vector<int>& arr, int num1, int num2) {
+    // Your code here
+    int index1 = -1;
+    int index2 = -1;
+
+    // leftmost occurence of num1 
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==num1){
+            index1 = i;
+            break;
+        }
+    }
+    // rightmost occurence of num2
+    for(int i=arr.size()-1;i>=0;i--){
+        if(arr[i]==num2){
+            index2 = i;
+            break;
+        }
+    }
+
+    return abs(index2-index1)-1;
+}
