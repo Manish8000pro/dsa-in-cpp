368e072f330116e4ba5b70af513326d8e29c66fb
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-05 16:25:39 +0530

    solution of this qusetion

--- Practice Questions/4. First Repeating Element.cpp
+++ Practice Questions/4. First Repeating Element.cpp
@@ -1,1 +1,16 @@
 //14. First Repeating Element
+int firstRepeatingElement(vector<int>& arr) {
+    // Your code here
+    unordered_map<int,int>freq;
+
+    for(int x : arr){
+        freq[x]++;
+    }
+    // find the first repeating element 
+    for(int i=0;i<arr.size();i++){
+        if(freq[arr[i]]>1){
+            return i+1;
+        }
+    }
+    return -1;
+}
