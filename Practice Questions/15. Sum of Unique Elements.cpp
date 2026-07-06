8647eae6ccd70f30c761048e2214b86cd2661658
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-06 14:39:22 +0530

    solution of the qusetion 15

--- Practice Questions/15. Sum of Unique Elements.cpp
+++ Practice Questions/15. Sum of Unique Elements.cpp
@@ -1,1 +1,17 @@
 // 15. Sum of Unique Elements
+int sumOfUnique(vector<int>& nums) {
+    // Your code here
+    unordered_map<int,int>freq;
+    // finnd freq 
+    for(int x:nums){
+        freq[x]++;
+    }
+
+    int sum = 0;
+    for(int x:nums){
+        if(freq[x]==1){
+            sum+=x;
+        }
+    }
+    return sum;
+}
