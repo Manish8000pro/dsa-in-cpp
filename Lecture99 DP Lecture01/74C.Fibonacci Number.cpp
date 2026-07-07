d8450fc2d163d3b06644a5f9ae980845452f1c44
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-07 12:57:16 +0530

    fibonacci solution with bottom up appro.

--- Lecture99 DP Lecture01/74C.Fibonacci Number.cpp
+++ Lecture99 DP Lecture01/74C.Fibonacci Number.cpp
@@ -1,1 +1,17 @@
 // 74C. Fibonacci Number using bottom up approach
+int fib(int n) {
+    // Your code here
+    vector<int>dp(n+1);
+
+    if(n<=1)
+    return n;
+
+    // base case fill in DP 
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
