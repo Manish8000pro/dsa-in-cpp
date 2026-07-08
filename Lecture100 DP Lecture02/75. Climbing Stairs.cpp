9beb3624bcf0c28e8b7ce7a057868c2a9833a597
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-08 14:59:24 +0530

    2nd solution of Climb stairs

--- Lecture100 DP Lecture02/75. Climbing Stairs.cpp
+++ Lecture100 DP Lecture02/75. Climbing Stairs.cpp
@@ -28,3 +28,30 @@ int climbStairs(int n) {
 
 //-----------------------------------------------------------
 // 2nd solution using DP
+int climb(int n,vector<int>&dp){
+    if(n<=2)
+    return n;
+
+    // Base case ko handle krte h 
+    if(dp[n]!=-1){
+        return n;
+    }
+
+    return dp[n] = climb(n-1,dp)+climb(n-2,dp);
+}
+
+int climbStairs(int n) {
+    // Your code here
+    if(n<=2)
+    return n;
+
+    vector<int>dp(n+1,-1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i=3;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+
+    return dp[n];
+}
