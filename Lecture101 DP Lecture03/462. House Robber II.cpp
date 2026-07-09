29e8aa6d625be9f1b3289d9948d3cef40401c139
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-09 21:37:35 +0530

    Best solution of Robber House

--- Lecture101 DP Lecture03/462. House Robber II.cpp
+++ Lecture101 DP Lecture03/462. House Robber II.cpp
@@ -1,2 +1,41 @@
 // 462. House Robber II
 // Best solution optimized solution , 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // code here
+        int n = nums.size();
+
+        if(n==1)
+        return nums[0] ;
+        
+        if(n==2)
+        return max(nums[0],nums[1]);
+
+        vector<int>dp(3,0);
+
+        dp[1] = nums[0];
+        dp[2] = max(nums[0],nums[1]);
+
+        // 0 to n-2 : ignore the last index 
+        for(int i=2;i<n-1;i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = max(nums[i]+dp[0],dp[1]);
+        }
+        int result = dp[2];
+        // we got the ans of  first case 0 to n-2
+
+        // 1 to n-1: ignore the first index 
+        dp[1] = nums[1];
+        dp[2] = max(nums[1],nums[2]);
+
+        for(int i=3;i<n;i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = max(nums[i]+dp[0],dp[1]);
+        } 
+        result = max(result,dp[2]);
+        return result;
+    }
+};
