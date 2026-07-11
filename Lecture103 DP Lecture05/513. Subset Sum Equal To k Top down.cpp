c8931a389843cfe653310d146aa9a403c8d583a2
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-11 18:14:04 +0530

    second solution with dp

--- Lecture103 DP Lecture05/513. Subset Sum Equal To k Top down.cpp
+++ Lecture103 DP Lecture05/513. Subset Sum Equal To k Top down.cpp
@@ -1,2 +1,25 @@
 // SubsetSum Equal to k 
 // Top bottom approach 
+class Solution {
+public:
+    bool findSubset(int index ,int sum,vector<int>&arr,vector<vector<int>>&dp){
+
+        if(sum==0)
+        return 1;
+
+        if(sum<0 || index<0)
+        return 0;
+
+        if(dp[index][sum]!=-1){
+            return dp[index][sum];
+        }
+        
+        return dp[index][sum] = findSubset(index-1,sum,arr,dp) || findSubset(index-1,sum-arr[index],arr,dp);
+    }
+
+    bool subsetSumToK(int n, int k, vector<int>& arr) {
+        // Your code here
+        vector<vector<int>>dp(n,vector<int>(k+1,-1));
+        return findSubset(n-1,k,arr,dp);
+    }
+};
