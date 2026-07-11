dc3165103afdd5f2c1aca4c6574d726aa3b11fee
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-11 12:27:37 +0530

    513 solution with recursive approach

--- Lecture103 DP Lecture05/513. Subset Sum Equal To K.cpp
+++ Lecture103 DP Lecture05/513. Subset Sum Equal To K.cpp
@@ -1,2 +1,20 @@
 //513. Subset Sum Equal To K
 // Recursive solution
+class Solution {
+public:
+    bool findSubset(int index ,int sum,vector<int>&arr){
+
+        if(sum==0)
+        return 1;
+
+        if(sum<0 || index<0)
+        return 0;
+        
+        return findSubset(index-1,sum,arr) || findSubset(index-1,sum-arr[index],arr);
+    }
+
+    bool subsetSumToK(int n, int k, vector<int>& arr) {
+        // Your code here
+        return findSubset(n-1,k,arr);
+    }
+};
