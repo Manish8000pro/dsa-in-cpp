1cde1350f5ff6eca3c76ac7426758ca613e75480
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-13 17:52:08 +0530

    Question is solved

--- Practice Questions/21. Cube Root of a Number.cpp
+++ Practice Questions/21. Cube Root of a Number.cpp
@@ -1,1 +1,25 @@
 //21. Cube Root of a Number
+int cubicRoot(int n) {
+    // Your code here
+    int left = 1;
+    int right = n;
+    int ans = 0;
+
+    while(left<=right){
+        long long mid = left+(right-left)/2;
+
+        long long cube = mid*mid*mid;
+
+        if(cube==n){
+            return mid;
+        }
+        else if(cube<n){
+            ans = mid;
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    return ans;
+}
