4bb09fa08c37926990efdd7d326a81ce5f6e635b
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-14 16:40:31 +0530

    removed unnecessery code

--- Lecture104 DP Lecture06/513. Subset Sum Equal To K Bottom.cpp
+++ Lecture104 DP Lecture06/513. Subset Sum Equal To K Bottom.cpp
@@ -1,20 +1,6 @@
 // 513. Subset Sum Equal To K
 class Solution {
 public:
-    bool findSubset(int index ,int sum,vector<int>&arr,vector<vector<int>>&dp){
-
-        if(sum==0)
-        return 1;
-
-        if(sum<0 || index<0)
-        return 0;
-
-        if(dp[index][sum]!=-1){
-            return dp[index][sum];
-        }
-        
-        return dp[index][sum] = findSubset(index-1,sum,arr,dp) || findSubset(index-1,sum-arr[index],arr,dp);
-    }
 
     bool subsetSumToK(int n, int k, vector<int>& arr) {
         // Your code here
