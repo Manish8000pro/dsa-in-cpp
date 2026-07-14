8eda10622f0a81109cebd379e2ae5aa25e842a0e
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-14 16:51:12 +0530

    best solution

--- Lecture104 DP Lecture06/513. Subset Sum Equal To K space further.cpp
+++ Lecture104 DP Lecture06/513. Subset Sum Equal To K space further.cpp
@@ -1,1 +1,23 @@
 // 513 Subset sum Equal To K further sapce optimization
+class Solution {
+public:
+
+    bool subsetSumToK(int n, int k, vector<int>& arr) {
+        // Your code here
+        vector<int>curr(k+1,0); 
+
+        curr[0]=1;
+
+        if(arr[0]<=k)
+        curr[arr[0]] = 1;
+        // first row and first column fill kr dia h 
+
+        for(int i=1;i<n;i++){
+            for(int j=k;j>0;j--){
+                if(j-arr[i]>=0)
+                curr[j] = curr[j] || curr[j-arr[i]];
+            }
+        }
+        return curr[k];
+    }
+};
