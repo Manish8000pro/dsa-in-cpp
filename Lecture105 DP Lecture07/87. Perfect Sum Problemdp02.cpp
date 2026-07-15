d248641410b637b3d4c1e55a6183686dbae98c39
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-15 16:23:13 +0530

    solution 02

--- Lecture105 DP Lecture07/87. Perfect Sum Problemdp02.cpp
+++ Lecture105 DP Lecture07/87. Perfect Sum Problemdp02.cpp
@@ -1,2 +1,25 @@
 //87. Perfect Sum Problem 
-// Dp second solution 
+// Dp second solution
+class Solution {
+public:
+    int findSubset(int n, int target,vector<int>&arr,vector<vector<int>>&dp){
+
+        if(n==0)
+        return target == 0;
+        
+        if(target<0)
+        return 0;
+
+        if(dp[n][target]!=-1)
+        return dp[n][target];
+
+        return dp[n][target] = findSubset(n-1,target,arr,dp)+findSubset(n-1,target-arr[n-1],arr,dp);
+    }
+
+    int countSubsetsWithSum(vector<int>& arr, int target) {
+        // Your code here
+        int n = arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        return findSubset(n,target,arr,dp);
+    }
+}; 
