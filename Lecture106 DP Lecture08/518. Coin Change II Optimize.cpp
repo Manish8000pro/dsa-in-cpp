b516f9dd2e9d9b47a5c3065c40fe7228cbc106e3
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-18 18:22:02 +0530

    Optimized solution

--- Lecture106 DP Lecture08/518. Coin Change II Optimize.cpp
+++ Lecture106 DP Lecture08/518. Coin Change II Optimize.cpp
@@ -1,2 +1,29 @@
 //518. Coin Change II
 // Optimize approach
+class Solution {
+public:
+
+    int change(int amount, vector<int>& coins) {
+        // Your code here
+        int n = coins.size();
+
+        vector<int>prev(amount+1,0);
+        prev[0] = 1;
+
+        
+        // 0th row and 0th colum ko fill kr liya h INitilize kr liya h 
+        
+        for(int i=1;i<=n;i++){
+            vector<int>curr(amount+1,0);
+            curr[0] = 1;
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0)
+                curr[j] = prev[j]+curr[j-coins[i-1]];
+                else
+                curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[amount];
+    }
+};
