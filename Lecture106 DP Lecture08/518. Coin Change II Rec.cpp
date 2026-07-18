70a472cc6bdd3b144b5597066fded0cc3926bcd9
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-18 16:30:57 +0530

    sol. of the 518

--- Lecture106 DP Lecture08/518. Coin Change II Rec.cpp
+++ Lecture106 DP Lecture08/518. Coin Change II Rec.cpp
@@ -1,2 +1,22 @@
 //518. Coin Change II
 // Recursive approach
+class Solution {
+public:
+    int totalWays(int n,int amount,vector<int>&coins){
+        if(amount==0)
+        return 1;
+
+        if(n==0 || amount<0){
+            return 0;
+        }
+
+        return totalWays(n-1,amount,coins)+totalWays(n,amount-coins[n-1],coins);
+    }
+
+    int change(int amount, vector<int>& coins) {
+        // Your code here
+        int n = coins.size();
+
+        return totalWays(n,amount,coins);
+    }
+};
