6c324d1c1a272f9738288f195fe9bf0c6654d230
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-18 18:08:56 +0530

    solution by bottom up approach

--- Lecture106 DP Lecture08/518. Coin Change II Bottom.cpp
+++ Lecture106 DP Lecture08/518. Coin Change II Bottom.cpp
@@ -1,2 +1,28 @@
 //518. Coin Change II
 // Bottom up approach
+class Solution {
+public:
+
+    int change(int amount, vector<int>& coins) {
+        // Your code here
+        int n = coins.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+
+        // first column ko fill kr do
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        // 0th row and 0th colum ko fill kr liya h INitilize kr liya h 
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0)
+                dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+                else
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
+    }
+};
