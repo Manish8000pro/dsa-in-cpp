c8190e30d69af543dfecc624ec7b91a8b0f01a96
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-18 18:33:46 +0530

    Best solution

--- Lecture106 DP Lecture08/518. Coin Change II Best .cpp
+++ Lecture106 DP Lecture08/518. Coin Change II Best .cpp
@@ -2,3 +2,22 @@
 // Best optimize solution 
 // TimeC O(n)
 // space C. o(amount)
+class Solution {
+public:
+
+    int change(int amount, vector<int>& coins) {
+        // Your code here
+        int n = coins.size();
+        vector<int>curr(amount+1,0);
+
+        curr[0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0)
+                curr[j] = curr[j]+curr[j-coins[i-1]];
+            }
+        }
+        return curr[amount];
+    }
+};
