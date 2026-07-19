4160447418cb38c12e39435b8e820c7668c3b9d7
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-19 17:33:05 +0530

    solution with the Top down app.

--- Lecture107 DP Lecture09/248. Coin Change TOp.cpp
+++ Lecture107 DP Lecture09/248. Coin Change TOp.cpp
@@ -1,2 +1,26 @@
 // 248. Coin Change
 // Top down approach
+class Solution {
+public:
+    int findMinCoins(int n,int amount,vector<int>&coins,vector<vector<int>>&dp){
+        if(amount==0)
+        return 0;
+
+        if(n==0 || amount<0)
+        return 1e9;
+
+        if(dp[n][amount]!=-1)
+        return dp[n][amount];
+
+        return dp[n][amount] = min(findMinCoins(n-1,amount,coins,dp), 1+findMinCoins(n,amount-coins[n-1],coins,dp));
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        // Your code here
+        int n = coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+
+        int answer = findMinCoins(n,amount,coins,dp);
+        return answer>=1e9 ? -1 : answer;
+    }
+};
