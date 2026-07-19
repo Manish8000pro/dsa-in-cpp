c823a6d5b3dac02acd1ff767839183f1cbb26058
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-19 19:06:52 +0530

    sol.

--- Lecture107 DP Lecture09/521. Combination Sum IVrecursive.cpp
+++ Lecture107 DP Lecture09/521. Combination Sum IVrecursive.cpp
@@ -1,1 +1,25 @@
 //521. Combination Sum IV
+class Solution {
+public:
+    int findTotalCombination(int n,int target,vector<int>&nums){
+        if(target==0)
+        return 1;
+
+        if(target<0)
+        return 0;
+
+        int result = 0;
+
+        for(int j=0;j<n;j++){
+            result+=findTotalCombination(n,target-nums[j],nums);
+        }
+        return result;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        // Your code here
+        int n = nums.size();
+
+        return findTotalCombination(n,target,nums);
+    }
+};
