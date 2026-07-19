a32c405a9c71de02bbf3ec9d7cea0e7dd435fc0d
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-19 17:21:17 +0530

    solve with the recursive

--- Lecture107 DP Lecture09/248. Coin Change recursive.cpp
+++ Lecture107 DP Lecture09/248. Coin Change recursive.cpp
@@ -1,2 +1,22 @@
 // 248. Coin Change 
 // recursive approach
+class Solution {
+public:
+    int findMinCoins(int n,int amount,vector<int>&coins){
+        if(amount==0)
+        return 0;
+
+        if(n==0 || amount<0)
+        return 1e9;
+
+        return min(findMinCoins(n-1,amount,coins), 1+findMinCoins(n,amount-coins[n-1],coins));
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        // Your code here
+        int n = coins.size();
+
+        int answer = findMinCoins(n,amount,coins);
+        return answer>=1e9 ? -1 : answer;
+    }
+};
