4803e1a5ab7a89d91887b0688c7c1b8dc97d96b2
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-19 18:31:38 +0530

    solved with the optimized way

--- Lecture107 DP Lecture09/248. Coin Change Optimize.cpp
+++ Lecture107 DP Lecture09/248. Coin Change Optimize.cpp
@@ -1,2 +1,30 @@
 //248. Coin Change
 // Optimized solution 
+class Solution {
+public:
+
+    int coinChange(vector<int>& coins, int amount) {
+        // Your code here
+        int n = coins.size();
+        vector<int>prev(amount+1,-1);
+
+        for(int j=0;j<=amount;j++){
+            prev[j] = 1e9;
+        }
+
+        prev[0] = 0;
+
+        for(int i=1;i<=n;i++){
+            vector<int>curr(amount+1);
+            curr[0] = 0;
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0)
+                curr[j] = min(prev[j],1+curr[j-coins[i-1]]);
+                else
+                curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[amount]>=1e9 ? -1 : prev[amount];
+    }
+};
