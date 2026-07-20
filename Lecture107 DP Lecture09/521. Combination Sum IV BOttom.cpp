4753261935ab01bebe3dddc3a48108f70d11eb54
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-20 14:38:20 +0530

    solution 521

--- Lecture107 DP Lecture09/521. Combination Sum IV BOttom.cpp
+++ Lecture107 DP Lecture09/521. Combination Sum IV BOttom.cpp
@@ -1,2 +1,23 @@
 //521. Combination Sum IV 
 // Bottom up 
+class Solution {
+public:
+
+    int combinationSum4(vector<int>& nums, int target) {
+        // Your code here
+        int n = nums.size();
+        vector<int>dp(target+1,-1);
+
+        dp[0] = 1;
+
+        for(int i=1;i<=target;i++){
+            int result = 0;
+            for(int j=0;j<n;j++){
+                if(i-nums[j]>=0)
+                result+=dp[i-nums[j]];
+            }
+            dp[i] = result;
+        }
+        return dp[target];
+    }
+};
