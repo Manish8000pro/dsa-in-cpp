087d0f2fb90a138d6e3f27e5bd7fee3f88ac9270
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-20 15:42:47 +0530

    solution of Q. 541

--- Lecture107 DP Lecture09/541. Perfect Squares.cpp
+++ Lecture107 DP Lecture09/541. Perfect Squares.cpp
@@ -1,1 +1,22 @@
 // 541. Perfect Squares 
+class Solution {
+public:
+    int numSquares(int n) {
+        // Your code here
+        if(n<=3)
+        return n;
+
+        vector<int>MinimumPerfectSqure(n+1,INT_MAX);
+        MinimumPerfectSqure[0] = 0;
+        MinimumPerfectSqure[1] = 1;
+        MinimumPerfectSqure[2] = 2;
+        MinimumPerfectSqure[3] = 3;
+
+        for(int i=4;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                MinimumPerfectSqure[i] = min(MinimumPerfectSqure[i],1+MinimumPerfectSqure[i-j*j]);
+            }
+        }
+        return MinimumPerfectSqure[n];
+    }
+};
