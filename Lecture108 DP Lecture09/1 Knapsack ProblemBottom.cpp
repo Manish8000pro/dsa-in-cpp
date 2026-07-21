51c6f0cfe8438eefdc3ec5c0182cfb0caffc4336
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-21 07:34:40 +0530

    hello

--- Lecture108 DP Lecture09/1 Knapsack ProblemBottom.cpp
+++ Lecture108 DP Lecture09/1 Knapsack ProblemBottom.cpp
@@ -32,7 +32,7 @@ class Solution {
                 dp[i][j] = max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
                 else
                 dp[i][j] = dp[i-1][j];
-            }
+            } 
         }
 
         return dp[n][W];
