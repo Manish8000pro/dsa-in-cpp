08935576a4f475d7419dfce4948f2975adfef182
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-21 15:35:56 +0530

    solved by dp

--- Lecture108 DP Lecture10/519. Rod Cutting Dp.cpp
+++ Lecture108 DP Lecture10/519. Rod Cutting Dp.cpp
@@ -1,2 +1,26 @@
 // 519. Rod Cutting 
 // solution with DP
+class Solution {
+public:
+    int cutRod(vector<int>& price, int n) {
+        // Your code here
+        if(n==0)
+        return 0;
+
+        if(n==1)
+        return price[0];
+
+        vector<int>dp(n+1,0);
+
+        for(int i=1;i<=n;i++){
+            dp[i] = price[i-1];
+        }
+
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i/2;j++){
+                dp[i] = max(dp[i],dp[j]+dp[i-j]);
+            }
+        }
+        return dp[n];
+    }
+};
