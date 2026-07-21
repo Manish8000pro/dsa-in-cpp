ec6650aa7969c0f192398651831898e0003cb49a
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-21 15:10:41 +0530

    solution by recursive method

--- Lecture108 DP Lecture10/519. Rod Cutting recur.cpp
+++ Lecture108 DP Lecture10/519. Rod Cutting recur.cpp
@@ -1,2 +1,21 @@
 // 519. Rod Cutting
 // recursive appr.
+class Solution {
+public:
+    int maxProfit(int n,vector<int>&price){
+        if(n==0)
+        return 0;
+
+        int result = 0;
+
+        for(int j=1;j<=n;j++){
+            result = max(result,price[j-1]+maxProfit(n-j,price));
+        }
+        return result;
+    }
+
+    int cutRod(vector<int>& price, int n) {
+        // Your code here
+        return maxProfit(n,price);
+    }
+};
