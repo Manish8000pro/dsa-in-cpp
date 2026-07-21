0c429222bab62f1eee728bd104f6b19df0ad8f34
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-21 16:01:36 +0530

    sol of Q. 282

--- Lecture109 DP Lecture11/282. Best Time to Buy and Sell Stock I.cpp
+++ Lecture109 DP Lecture11/282. Best Time to Buy and Sell Stock I.cpp
@@ -1,1 +1,14 @@
 //282. Best Time to Buy and Sell Stock I
+int maxProfit(vector<int>& prices) {
+    int n = prices.size(), prevSmallStockPrices = prices[0],profit = 0;
+
+    for(int i=1;i<n;i++){
+        if(prices[i]>prevSmallStockPrices){
+            profit = max(profit,prices[i]-prevSmallStockPrices);
+        }
+        else{
+            prevSmallStockPrices = prices[i];
+        }
+    }
+    return profit;
+}
