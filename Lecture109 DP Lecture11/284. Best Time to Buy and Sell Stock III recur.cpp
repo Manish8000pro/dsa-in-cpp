4208a9360261bd3b17a282b6295ac18e0c13641f
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-21 16:50:12 +0530

    recursive solution of the Q.

--- Lecture109 DP Lecture11/284. Best Time to Buy and Sell Stock III recur.cpp
+++ Lecture109 DP Lecture11/284. Best Time to Buy and Sell Stock III recur.cpp
@@ -1,2 +1,23 @@
 //284. Best Time to Buy and Sell Stock III 
 // recursive approach
+int maximumProfit(int index,int buy,int transactions,int n,vector<int>&prices){
+    if(transactions == 0 || index == n){
+        return 0;
+    }
+    
+
+    if(buy){
+        // Either i can buy the stock or i will not buy the stock 
+
+        return max(-prices[index]+maximumProfit(index+1,0,transactions,n,prices),maximumProfit(index+1,1,transactions,n,prices));
+    }
+    else{
+        // Either I can sell the stock or I will not sell the stock 
+        return max(prices[index]+maximumProfit(index+1,1,transactions-1,n,prices),maximumProfit(index+1,0,transactions,n,prices));
+    }
+}
+
+int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    return maximumProfit(0,1,2,n,prices);
+}
