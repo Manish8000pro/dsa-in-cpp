db5eaeb57594c62fd63c917884d3bcacecc06d87
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-22 19:20:25 +0530

    solution of the Question with space opti.

--- Lecture109 DP Lecture11/285. Best Time to Buy and Sell Stock IV.cpp
+++ Lecture109 DP Lecture11/285. Best Time to Buy and Sell Stock IV.cpp
@@ -1,1 +1,21 @@
 //285. Best Time to Buy and Sell Stock IV
+int maxProfit(int transaction, vector<int>& prices) {
+    int n = prices.size();
+    vector<vector<int>>prev(2,vector<int>(transaction+1,0));
+    
+    for(int i=n-1;i>=0;i--){
+        vector<vector<int>>curr(2,vector<int>(transaction+1,0));
+        for(int j=0;j<2;j++){
+            for(int k=1;k<transaction+1;k++){
+                if(j==1){
+                    curr[j][k] = max(-prices[i]+prev[0][k],prev[1][k]);
+                }
+                else{
+                    curr[j][k] = max(prices[i]+prev[1][k-1],prev[0][k]);
+                }
+            }
+        }
+        prev = curr;
+    }
+    return prev[1][transaction];
+}
