2599c44a500798bf491572d8122ffe8b5057c15b
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-24 14:47:40 +0530

    solution

--- Lecture110 DP Lecture12/286. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ Lecture110 DP Lecture12/286. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,1 +1,12 @@
 //286. Best Time to Buy and Sell Stock with Cooldown.cpp
+int maxProfit(vector<int>& prices) {
+    int maximumProfit = -prices[0],n=prices.size();
+
+    vector<int>dp(n+1,0);
+
+    for(int i=2;i<=n;i++){
+        dp[i] = max(dp[i-1],prices[i-1]+maximumProfit);
+        maximumProfit = max(maximumProfit,dp[i-2]-prices[i-1]);
+    }
+    return dp[n];
+}
