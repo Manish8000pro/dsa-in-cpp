9f1aee60b3f516bc09e73ecee1c9271616323647
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-25 17:42:49 +0530

    solution with sapce optimized approach

--- Lecture111 DP Lecture13/255. Longest Common Subsequence Space.cpp
+++ Lecture111 DP Lecture13/255. Longest Common Subsequence Space.cpp
@@ -1,2 +1,19 @@
 // 255. Longest Common Subsequence
 // space optimize approach
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.size(), n = text2.size();
+    vector<int>prev(n+1,0);
+
+    for(int i=1;i<=m;i++){
+        vector<int>curr(n+1,0);
+        for(int j=1;j<=n;j++){
+            if(text1[i-1]== text2[j-1])
+            curr[j] = 1+prev[j-1];
+            else
+            curr[j] = max(curr[j-1],prev[j]);
+        }
+        prev = curr;
+    }
+    
+    return prev[n];
+}
