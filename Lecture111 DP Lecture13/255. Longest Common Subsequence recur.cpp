cfdf9a07be34a4ac7bb15fb5f494653867139371
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-25 16:21:05 +0530

    first solution of 255

--- Lecture111 DP Lecture13/255. Longest Common Subsequence recur.cpp
+++ Lecture111 DP Lecture13/255. Longest Common Subsequence recur.cpp
@@ -1,2 +1,19 @@
 //255. Longest Common Subsequence
 // recursive solution , This gives the runtime error
+int LCS(int m,int n,string &text1,string &text2 ){
+    if(m==0 || n==0)
+    return 0;
+
+    // char will match and they will not match 
+    if(text1[m-1] == text2[n-1]){
+        return 1+LCS(m-1,n-1,text1,text2);
+    }
+    else{
+        return max(LCS(m,n-1,text1,text2),LCS(m-1,n,text1,text2));
+    }
+}
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.size(), n = text2.size();
+    
+    return LCS(m,n,text1,text2);
+}
