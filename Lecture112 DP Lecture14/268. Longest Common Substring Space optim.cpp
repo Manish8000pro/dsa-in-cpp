4ae52f5a26ad48e3cc2f42161406b8cb6d691c24
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-27 16:18:37 +0530

    solution of LC Substring

--- Lecture112 DP Lecture14/268. Longest Common Substring Space optim.cpp
+++ Lecture112 DP Lecture14/268. Longest Common Substring Space optim.cpp
@@ -1,2 +1,21 @@
 //268. Longest Common Substring Space optimization 
 // TOp soultion of LCSubstring
+int longestCommonSubstr(string s1, string s2) {
+
+    int m = s1.size(),n = s2.size(),result = 0;
+    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            int answer = 0;
+            if(s1[i-1]==s2[j-1]){
+            answer = 1+dp[i-1][j-1];
+            result = max(answer,result);
+            }
+            dp[i][j] = answer;
+        }
+        
+    }
+
+    return result;
+}
