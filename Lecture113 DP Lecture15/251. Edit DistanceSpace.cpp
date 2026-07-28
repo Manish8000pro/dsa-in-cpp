7afc8342b3e6f95f113fd56dcad74ab13a3c84e4
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-28 10:05:22 +0530

    space optimized solution

--- Lecture113 DP Lecture15/251. Edit DistanceSpace.cpp
+++ Lecture113 DP Lecture15/251. Edit DistanceSpace.cpp
@@ -1,2 +1,29 @@
 //251. Edit Distance
 // space optimized
+// bottom Up
+// m : m to 0 : i
+// n : n to 0 : j
+
+int minDistance(string word1, string word2) {
+    int m = word1.size(),n = word2.size();
+    // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    vector<int>prev(n+1,0);
+
+    // Row zero ko fill kr do 
+    for(int j=0;j<=n;j++){
+        prev[j] = j;
+    }
+
+    for(int i=1;i<=m;i++){
+        vector<int>curr(n+1,0);
+        curr[0] = i;
+        for(int j=1;j<=n;j++){
+            if(word1[i-1]==word2[j-1])
+            curr[j] = prev[j-1];
+            else
+            curr[j] = 1+min( prev[j],min(curr[j-1],prev[j-1]));
+        }
+        prev = curr;
+    }
+    return prev[n];
+}
