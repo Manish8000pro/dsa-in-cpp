24ce2d20f6620280ab3be055167aef41eeea9078
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-28 17:47:43 +0530

    best solution

--- Lecture113 DP Lecture15/526. Distinct SubsequencesSpaceOpti.cpp
+++ Lecture113 DP Lecture15/526. Distinct SubsequencesSpaceOpti.cpp
@@ -1,2 +1,29 @@
 //526. Distinct Subsequences 
 // space otimized
+class Solution {
+public:
+
+    int numDistinct(string s, string t) {
+        // Your code here
+        int m = s.size(),n = t.size();
+
+        
+
+        vector<unsigned long long>prev(n+1,0);
+
+        prev[0] = 1;
+
+        for(int i=1;i<=m;i++){
+            vector<unsigned long long>curr(n+1,0);
+            curr[0] =1;
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1])
+                curr[j] = prev[j-1]+prev[j];
+                else
+                curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[n];
+    }
+};
