a4777c2614dfeebbeda286e9ea568652daa25741
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-29 15:53:02 +0530

    Solution of 259 pro.

--- Lecture114 DP Lecture 16/259. Max Sum Increasing Subsequence.cpp
+++ Lecture114 DP Lecture 16/259. Max Sum Increasing Subsequence.cpp
@@ -1,1 +1,17 @@
 //259. Max Sum Increasing Subsequence
+int maxSumIncreasingSubsequence(vector<int> &arr) {
+    int n = arr.size();
+    vector<int>MSIS = arr;
+
+    int result = arr[0];
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i] > arr[j]){
+                MSIS[i] = max(MSIS[i],arr[i]+MSIS[j]);
+            }
+        }
+        result = max(MSIS[i],result);
+    }
+    return result;
+}
