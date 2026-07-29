14eff762927972234879f4352a36f22f0ee4983c
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-29 15:11:13 +0530

    solution of 529

--- Lecture114 DP Lecture 16/529. Print Longest Increasing Subsequence.cpp
+++ Lecture114 DP Lecture 16/529. Print Longest Increasing Subsequence.cpp
@@ -1,1 +1,34 @@
 //529. Print Longest Increasing Subsequence
+class Solution {
+public:
+    vector<int> longestIncreasingSubsequence(vector<int>& arr) {
+        // Your code here
+        int n = arr.size();
+        vector<int>LIS(n,1);
+        vector<int>Parent(n,-1);
+
+        int result = 1,index = 0;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]&&(LIS[i]<1+LIS[j])){
+                    LIS[i] = 1+LIS[j];
+                    Parent[i] = j;
+                }
+            }
+            if(result<LIS[i]){
+                result = LIS[i];
+                index = i;
+            }
+        }
+        
+        vector<int>answer;
+        while(index!=-1){
+            answer.push_back(arr[index]);
+            index = Parent[index];
+        }
+        reverse(answer.begin(),answer.end());
+
+        return answer;
+    }
+};
