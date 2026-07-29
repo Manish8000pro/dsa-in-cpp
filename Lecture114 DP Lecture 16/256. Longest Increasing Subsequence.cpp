2fdb66669b6a9ba8f12e6fe47c997ae09f7f3464
Manish8000pro <manishkumawat6227@gmail.com>
2026-07-29 14:24:09 +0530

    solution of the Ques.

--- Lecture114 DP Lecture 16/256. Longest Increasing Subsequence.cpp
+++ Lecture114 DP Lecture 16/256. Longest Increasing Subsequence.cpp
@@ -1,1 +1,16 @@
 //256. Longest Increasing Subsequence
+int lengthOfLIS(vector<int>& nums) {
+    int n =  nums.size();
+    vector<int>LIS(n,1);
+
+    int result = 1;
+
+    for(int i=0;i<n;i++){
+        for(int j = i-1;j>=0;j--){
+            if(nums[j]<nums[i])
+            LIS[i] = max(LIS[i],1+LIS[j]);
+        }
+        result = max(LIS[i],result);
+    }
+    return result;
+}
