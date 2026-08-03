6ce48f0073f84f5430fd286f17edb66060f301e3
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-03 17:48:01 +0530

    solution with Top Down app.

--- Lecture117 DP Lecture19/262. Egg Dropping Puzzle.cpp
+++ Lecture117 DP Lecture19/262. Egg Dropping Puzzle.cpp
@@ -1,2 +1,27 @@
 //262. Egg Dropping Puzzle
 // top down approach 
+int findTotalFloor(int eggs ,int moves,vector<vector<int>>&dp){
+    if(eggs==0 || moves==0)
+    return 0;
+
+    if(eggs==1)
+    return  moves;
+
+    if(dp[eggs][moves]!=-1){
+        return  dp[eggs][moves];
+    }
+
+    return dp[eggs][moves] = 1+findTotalFloor(eggs-1,moves-1,dp)+findTotalFloor(eggs,moves-1,dp);
+}
+
+int eggDrop(int n, int k) {
+    
+    vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+
+    int moves = 0;
+
+    while(findTotalFloor(n,moves,dp)<k){
+        moves++;
+    }
+    return moves;
+}
