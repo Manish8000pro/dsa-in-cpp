72e9f4d03c9835170ba290bf401169ad92bebc82
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-03 17:32:21 +0530

    complete solution

--- Lecture117 DP Lecture19/262. Egg Dropping PuzzleBott.cpp
+++ Lecture117 DP Lecture19/262. Egg Dropping PuzzleBott.cpp
@@ -1,2 +1,18 @@
 //262. Egg Dropping Puzzle
 // bottom up approach with dp
+int eggDrop(int n, int k) {
+    vector<vector<int>>dp(n+1,vector<int>(k+1,0));
+
+    for(int j=0;j<=k;j++){
+        dp[1][j] = j;
+    }
+
+    for(int moves=1;moves<=k;moves++){
+        for(int eggs=2;eggs<=n;eggs++){
+            dp[eggs][moves] = 1+dp[eggs-1][moves-1]+dp[eggs][moves-1];
+        }
+        if(dp[n][moves]>=k)
+        return moves;
+    }
+    return k;
+}
