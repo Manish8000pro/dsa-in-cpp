6b7fd58cd8b7f154e667dac23224a985d33a6a72
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-04 16:38:48 +0530

    space optimized solution

--- Lecture117 DP Lecture19/549. Palindrome SubStrings Count.cpp
+++ Lecture117 DP Lecture19/549. Palindrome SubStrings Count.cpp
@@ -1,1 +1,30 @@
 //549. Palindrome SubStrings Count
+class Solution {
+public:
+    int countPS(string s) {
+        // Your code here
+        int n = s.size();
+
+        vector<vector<bool>>dp(n,vector<bool>(n,0));
+
+        // size ki string : unki DIagnol ko 1 kr do 
+
+        for(int i=0;i<n;i++){
+            dp[i][i] = 1;
+        }
+        int totalPallindrome = 0;
+
+        for(int len = 2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+
+                if(s[i]==s[j]&&(len==2 || dp[i+1][j-1])){
+                    dp[i][j] = 1;
+                    totalPallindrome++;
+                }
+            }
+        }
+        return totalPallindrome;
+
+    }
+};
