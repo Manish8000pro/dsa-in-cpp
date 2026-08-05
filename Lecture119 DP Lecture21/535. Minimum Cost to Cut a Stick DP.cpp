4e4859a3b7ada1534b85e5f8d10319803ae7f275
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-05 16:14:06 +0530

    Optimized solution

--- Lecture119 DP Lecture21/535. Minimum Cost to Cut a Stick DP.cpp
+++ Lecture119 DP Lecture21/535. Minimum Cost to Cut a Stick DP.cpp
@@ -1,3 +1,38 @@
 //535. Minimum Cost to Cut a Stick 
 // solution with DP 
 // Optimized solution wiht DP 
+class Solution {
+public:
+    int findMinCost(int start,int end,vector<int>&cuts,vector<vector<int>>&dp){
+        if(start+1 == end){
+            return 0;
+        }
+
+        if(dp[start][end]!=-1){
+            return dp[start][end];
+        }
+
+        // try all the cuts 
+        int result = INT_MAX;
+
+        for(int k=start+1;k<end;k++){
+            // LEft side   Right side 
+            int answer = cuts[end]-cuts[start] + findMinCost(start,k,cuts,dp) + findMinCost(k,end,cuts,dp);
+
+            result = min(answer,result);
+        }
+        return dp[start][end]= result;
+    }
+
+    int minCost(int n, vector<int>& cuts) {
+        // Your code here
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+
+        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),-1));
+
+        return findMinCost(0,cuts.size()-1,cuts,dp);
+    }
+};
+
