3d7fafa1ec077f9f2ff0ebe7d597908070b21e04
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-05 16:05:33 +0530

    solution

--- Lecture119 DP Lecture21/535. Minimum Cost to Cut a StickRecu2.cpp
+++ Lecture119 DP Lecture21/535. Minimum Cost to Cut a StickRecu2.cpp
@@ -1,2 +1,31 @@
 //535. Minimum Cost to Cut a Stick 
 // second recursive approach
+class Solution {
+public:
+    int findMinCost(int start,int end,vector<int>&cuts){
+        if(start+1 == end){
+            return 0;
+        }
+
+        // try all the cuts 
+        int result = INT_MAX;
+
+        for(int k=start+1;k<end;k++){
+            // LEft side   Right side 
+            int answer = cuts[end]-cuts[start] + findMinCost(start,k,cuts) + findMinCost(k,end,cuts);
+
+            result = min(answer,result);
+        }
+        return result;
+    }
+
+    int minCost(int n, vector<int>& cuts) {
+        // Your code here
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+
+        return findMinCost(0,cuts.size()-1,cuts);
+    }
+};
+
