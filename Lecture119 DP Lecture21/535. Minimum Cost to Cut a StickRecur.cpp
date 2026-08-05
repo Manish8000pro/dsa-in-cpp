298964b837986a19ccf7c6e070fb116e7eff0b33
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-05 15:49:46 +0530

    not Optimized

--- Lecture119 DP Lecture21/535. Minimum Cost to Cut a StickRecur.cpp
+++ Lecture119 DP Lecture21/535. Minimum Cost to Cut a StickRecur.cpp
@@ -1,3 +1,29 @@
 //535. Minimum Cost to Cut a Stick
 // Recursive solution 
 // this gives the runtime error because this is not Optimized solution 
+class Solution {
+public:
+    int findMinCost(int start,int end,int rodStart,int rodEnd,vector<int>&cuts){
+        if(start>end){
+            return 0;
+        }
+
+        // try all the cuts 
+        int result = INT_MAX;
+
+        for(int k=start;k<=end;k++){
+            // LEft side   Right side 
+            int answer = rodEnd-rodStart + findMinCost(start,k-1,rodStart,cuts[k],cuts) + findMinCost(k-1,end,cuts[k],rodEnd,cuts);
+
+            result = min(answer,result);
+        }
+        return result;
+    }
+
+    int minCost(int n, vector<int>& cuts) {
+        // Your code here
+        sort(cuts.begin(),cuts.end());
+
+        return findMinCost(0,cuts.size()-1,0,n,cuts);
+    }
+};
