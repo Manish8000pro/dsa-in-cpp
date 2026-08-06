9141ad48a3952197d4eb3f88cd85da2de23569e4
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-06 17:21:02 +0530

    solution

--- Practice Questions/8. Minimum Swaps and K Together.cpp
+++ Practice Questions/8. Minimum Swaps and K Together.cpp
@@ -2,3 +2,33 @@
 // This is the brute force approach 
 // Time complexity o(n2)
 // Space complexity is o(1)
+int minSwaps(vector<int>& arr, int k) {
+    int n = arr.size();
+
+    int good = 0;
+
+    for(int num: arr){
+        if(num<=k){
+            good++;
+        }
+    }
+
+    if(good==0 || good==1)
+    return 0;
+
+
+    int ans = INT_MAX;
+
+    for(int i =0;i<=n-good;i++){
+        int bad = 0;
+        for(int j=i;j<i+good;j++){
+            if(arr[j]>k){
+                bad++;
+            }
+        }
+        
+        ans = min(ans,bad);
+    }
+
+    return ans;
+}
