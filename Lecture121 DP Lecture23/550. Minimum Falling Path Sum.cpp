cb3e5690cddd997510ec747512c93738c28f0d88
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-08 16:22:09 +0530

    solution

--- Lecture121 DP Lecture23/550. Minimum Falling Path Sum.cpp
+++ Lecture121 DP Lecture23/550. Minimum Falling Path Sum.cpp
@@ -1,1 +1,25 @@
 //550. Minimum Falling Path Sum
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // Your code here
+        int n = matrix.size();
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int minChoice = matrix[i+1][j];
+                if(j>0) minChoice = min(minChoice,matrix[i+1][j-1]);
+                if(j<n-1) minChoice = min(minChoice,matrix[i+1][j+1]);
+
+                matrix[i][j]+=minChoice;
+            }
+        }
+
+        int result = INT_MAX;
+
+        for(int j=0;j<n;j++){
+            result = min(result,matrix[0][j]);
+        }
+        return result;
+    }
+};
