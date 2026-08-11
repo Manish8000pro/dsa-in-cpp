c5f9d899e28fd4e8497dd5f55c6b363457e97a95
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-11 17:34:31 +0530

    solution of Q.

--- Lecture122 DP Lecture24/650. Count Integers With Even Digit Sum.cpp
+++ Lecture122 DP Lecture24/650. Count Integers With Even Digit Sum.cpp
@@ -1,1 +1,16 @@
 //650. Count Integers With Even Digit Sum
+class Solution {
+public:
+    int countEven(int num) {
+        // Your code here
+        int sumOfDigit = 0, n = num;
+
+        while(n){
+            sumOfDigit+=(n%10);
+            n/=10;  
+        }
+
+        return sumOfDigit%2? (num-1)/2 : num/2;
+        
+    }
+};
