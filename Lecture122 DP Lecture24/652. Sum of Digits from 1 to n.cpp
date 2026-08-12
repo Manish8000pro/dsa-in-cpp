2dbf07850cb340200cc1692c357de580abd74d1f
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-12 16:17:08 +0530

    complete solution

--- Lecture122 DP Lecture24/652. Sum of Digits from 1 to n.cpp
+++ Lecture122 DP Lecture24/652. Sum of Digits from 1 to n.cpp
@@ -1,1 +1,23 @@
 //652. Sum of Digits from 1 to n
+class Solution {
+public:
+    long long sumOfDigits(long long n) {
+        // Your code here
+        if(n<10)
+        return n*(n+1)/2;
+
+        int power =1;
+        int digit = 0;
+
+        while(power*10<=n){
+            power*=10;
+            digit++;
+        }
+
+        long long sumElement = 45*digit*(power/10);
+        long long firstElement = n/power;
+        long long remainingElement = n%power;
+
+        return firstElement*(sumElement)+ ((firstElement*(firstElement-1))/2)*power+ firstElement*(remainingElement+1)+ sumOfDigits(remainingElement);
+    }
+};
