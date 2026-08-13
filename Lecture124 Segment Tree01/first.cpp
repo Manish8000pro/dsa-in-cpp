d4dbe2e25fb0d776a5f4854d0c526577ec6bfc36
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-13 16:02:06 +0530

    Segment tree

---
+++ Lecture124 Segment Tree01/first.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class SegmentTree{
+    public:
+    vector<int>tree;
+    int n;
+    // array ka size ha original wala 
+    SegmentTree(vector<int>&arr){
+        n = arr.size();
+        tree.resize(4*n);
+        // build tree(0,0,n-1,arr)
+        buildTree(0,0,n-1,arr);
+    }
+
+    void buildTree(int node,int start,int end,vector<int>&arr){
+        if(start==end){
+            tree[node] = arr[start];
+            return; 
+        }
+        
+        int mid = start+(end-start)/2;
+
+        buildTree(2*node+1,start,mid,arr);
+        buildTree(2*node+2,mid+1,end,arr);
+
+        tree[node] = tree[2*node+1]+tree[2*node+2];
+        
+    }
+}
+
+int main(){
+
+}
