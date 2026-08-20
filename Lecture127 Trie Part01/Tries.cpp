82625b6bf30acbd83f0fe8b417577151c892838b
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-20 16:03:13 +0530

    build a constructer

--- Lecture127 Trie Part01/Tries.cpp
+++ Lecture127 Trie Part01/Tries.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
-// build  a node 
+// how our Trie node will look like 
+class TrieNode{
+    public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode(){
+        isEnd = false;
+
+        for(int i = 0;i<26;i++){
+            children[i] = NULL;
+        }
+    }
+};
+
+class Trie{
+    public:
+
+    TrieNode*root;
+
+    Trie(){
+        root = new TrieNode();
+    }
+
+    // 
+}
 
 int main(){
 
