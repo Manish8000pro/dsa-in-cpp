7a69be8d18142b024cb835999dff7457bbae5b80
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-27 17:41:24 +0530

    making Trie class

--- Lecture129 Trie Part03/661. Maximum XOR of Two Numbers in an Array.cpp
+++ Lecture129 Trie Part03/661. Maximum XOR of Two Numbers in an Array.cpp
@@ -1,1 +1,16 @@
 // 661. Maximum XOR of Two Numbers in an Array
+class TrieNode{
+    public:
+
+    TrieNode *children[2];
+
+    TrieNode(){
+        children[0]=NULL;
+        children[1]=NULL;
+    }
+
+    ~TrieNode(){
+        delete children[0];
+        delete children[1];
+    }
+};
