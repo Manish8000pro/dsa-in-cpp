1fdb9ab2e82bfa7c387892d10a72a3aa6d2fbe65
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-31 16:41:47 +0530

    sorting

--- Lecture129 Trie Part03/662. Maximum XOR With an Element From Array.cpp
+++ Lecture129 Trie Part03/662. Maximum XOR With an Element From Array.cpp
@@ -47,3 +47,45 @@ public:
         }
         return answer;
     }   
+
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        // Your code here
+        TrieNode *root = new TrieNode();
+
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>> q;
+
+        for(int i=0;i<queries.size();i++){
+            q.push_back({queries[i][0],queries[i][1],i});
+        }
+
+        sort(q.begin(),q.end(),
+            [](vector<int>& a, vector<int>& b){
+                return a[1] < b[1];
+            });
+
+        vector<int> answer(queries.size());
+
+        int j = 0;
+
+        for(auto &query : q){
+
+            int x = query[0];
+            int m = query[1];
+            int index = query[2];
+
+            while(j < nums.size() && nums[j] <= m){
+                insert(root,nums[j]);
+                j++;
+            }
+
+            if(j == 0)
+                answer[index] = -1;
+            else
+                answer[index] = getMaxXOr(root,x);
+        }
+
+        return answer;
+    }
+};    
