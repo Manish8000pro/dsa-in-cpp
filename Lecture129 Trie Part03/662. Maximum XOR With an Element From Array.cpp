60f6c10a277ccfd2d6ba7fb5ddaa2a65bc894cf1
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-31 16:39:41 +0530

    Making trie node

--- Lecture129 Trie Part03/662. Maximum XOR With an Element From Array.cpp
+++ Lecture129 Trie Part03/662. Maximum XOR With an Element From Array.cpp
@@ -1,3 +1,17 @@
 //strike 
 662. Maximum XOR With an Element From Array  
- 
+class TrieNode{ 
+    public:
+
+    TrieNode *children[2];
+
+    TrieNode(){
+        children[0]=NULL;
+        children[1]=NULL;
+    }
+
+    ~TrieNode(){
+        delete children[0];
+        delete children[1];
+    }
+};
