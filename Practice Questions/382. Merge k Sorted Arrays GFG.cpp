186b99740291413000d7659b1b7c4d58a15c816a
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-01 22:42:06 +0530

    solved

--- Practice Questions/382. Merge k Sorted Arrays GFG.cpp
+++ Practice Questions/382. Merge k Sorted Arrays GFG.cpp
@@ -1,3 +1,42 @@
 //Merge k Sorted Arrays
 // This problme solved on GFG
 // GFG solution
+class Solution {
+  public:
+    vector<int> mergeArrays(vector<vector<int>> &mat) {
+        // Code here
+        int n = mat.size();
+
+        priority_queue<array<int,3>,vector<array<int,3>>,greater<array<int,3>>> pq;
+
+        vector<int>answer;
+
+        // put the first element every row
+
+        for(int row = 0;row<n;row++){
+            if(!mat[row].empty()){
+                pq.push({mat[row][0],row,0});
+            }
+        }
+
+        while(!pq.empty()){
+            // get smallest element
+            auto current = pq.top();
+            pq.pop();
+
+            int value = current[0];
+            int row = current[1];
+            int col = current[2];
+
+            answer.push_back(value);
+
+            // put next element from same row 
+            if(col+1<mat[row].size()){
+                pq.push({
+                    mat[row][col+1],row,col+1
+                });
+            }
+        }
+        return answer;
+    }
+};
