c4bb67f48e11a6d0ba811a67b7fa804200f009ce
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-03 08:42:59 +0530

    code

--- Practice Questions/396. Check Tree Traversal.cpp
+++ Practice Questions/396. Check Tree Traversal.cpp
@@ -61,3 +61,56 @@ public:
 
 // ==============================================================================================
 
+class Solution{
+public:
+    unordered_map<int,int>inorderIndex;
+
+    bool solve(int preorder[], int inorder[], int postorder[],int preStart,int inStart,int postStart,int len){
+        
+        if(len==0)
+        return true;
+
+        int root = preorder[preStart];
+        // root must also be the last in postorder
+
+        if(postorder[postStart+len-1]!=root)
+        return false;
+
+        // Root must exist in the current inorder range
+
+        if(inorderIndex.find(root)==inorderIndex.end())
+        return false;
+
+        int rootIndex = inorderIndex[root];
+
+        if(rootIndex<inStart || rootIndex>=inStart+len)
+        return false;
+
+        int leftSize = rootIndex - inStart;
+        int rightSize = len - leftSize - 1;
+
+        // check the left sub tree
+        bool leftValid = solve(preorder,inorder,postorder,preStart+1,inStart,postStart,leftSize);
+
+        if(!leftValid)
+        return false;
+
+        // check the right subTree
+        bool rightValid = solve(preorder,inorder,postorder,preStart+1+leftSize,rootIndex+1,postStart+leftSize,rightSize);
+
+        return rightValid;
+    }
+
+    bool checktree(int preorder[], int inorder[], int postorder[], int N) 
+    {
+    	// Your code here
+
+        inorderIndex.clear();
+
+        for(int i = 0; i < N; i++) {
+            inorderIndex[inorder[i]] = i;
+        }
+
+        return solve(preorder,inorder,postorder, 0, 0, 0, N);
+    }
+};
