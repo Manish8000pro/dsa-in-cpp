08aa2781af1e475efe35b8c9d21fb0d3c1bc5dfb
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-05 18:58:25 +0530

    special node's answer creating

--- Practice Questions/410. Max Path Sum 2 Special Nodes.cpp
+++ Practice Questions/410. Max Path Sum 2 Special Nodes.cpp
@@ -12,3 +12,30 @@ struct Node
     }
 };
 */
+class Solution {
+public:
+    int solve(Node* root , int &ans){
+        //if special node = leaf
+        if(!root->left && !root->right)
+        return root->data;
+
+        // both children exist 
+        if(root->left && root->right){
+            int leftSum = solve(root->left,ans);
+            int rightSum = solve(root->right,ans);
+
+            // complete special node to special  node path
+            ans = max(ans,leftSum+rightSum+root->data);
+
+            // Return one branch to parent
+            return root->data + max(leftSum, rightSum);
+        }
+
+        // Only left child
+        if (root->left)
+        return root->data + solve(root->left, ans);
+
+        //  Only right child 
+        
+        return root->data + solve(root->right,ans);
+    }
