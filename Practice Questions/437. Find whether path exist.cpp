1f2ac52429740b84ae8ff110e95b50ba79749134
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-07 14:54:20 +0530

    not optimized code

--- Practice Questions/437. Find whether path exist.cpp
+++ Practice Questions/437. Find whether path exist.cpp
@@ -1,3 +1,62 @@
 // 437.Find wheather path exist 
 // this is solution but this gives the runtime error
 // Stack overflow condition face because using DFS (depth first search)
+class Solution {
+  public:
+
+    int n;
+
+    int di[4] = {-1,1,0,0};
+    int dj[4] = {0,0,-1,1};
+
+    bool dfs(int i,int j,vector<vector<int>>&grid,vector<vector<bool>>&visited){
+        // outside
+
+        if(i<0 || i>=n || j<0 || j>=n)
+        return false;
+
+        // wall 
+        if(grid[i][j]==0)
+        return false;
+
+        // Destination
+
+        if(grid[i][j]==2)
+        return true;
+
+        visited[i][j]=true;
+
+        for(int k = 0;k<4;k++){
+            int ni = i+di[k];
+            int nj = j+dj[k];
+
+            if(dfs(ni,nj,grid,visited))
+            return true;
+        }
+        return false;
+    }
+
+    bool is_Possible(vector<vector<int>>& grid) {
+        // code here
+        n = grid.size();
+
+        // find where (source) is 
+
+        int si = -1;
+        int sj = -1;
+
+        // search every cell 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    si = i;
+                    sj = j;
+                }
+            }
+        }
+
+        vector<vector<bool>>visited(n,vector<bool>(n,false));
+        return dfs(si,sj,grid,visited);
+
+    }
+};
