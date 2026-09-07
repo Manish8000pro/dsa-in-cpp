116fd8ac5d0ef75efc59b371cd7f822b7f2e0888
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-07 14:53:47 +0530

    DFS

--- Practice Questions/437. Find whether path exist.cpp
+++ Practice Questions/437. Find whether path exist.cpp
@@ -1,3 +1,3 @@
 // 437.Find wheather path exist 
 // this is solution but this gives the runtime error
-// Stack overflow condition face
+// Stack overflow condition face because using DFS (depth first search)
