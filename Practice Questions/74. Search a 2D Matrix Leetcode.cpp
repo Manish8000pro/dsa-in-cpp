e678f9c99ee86eab930f51ac306677d30a414e4b
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-08 23:12:18 +0530

    complete code

--- Practice Questions/74. Search a 2D Matrix Leetcode.cpp
+++ Practice Questions/74. Search a 2D Matrix Leetcode.cpp
@@ -1,1 +1,28 @@
 //74. Search a 2D Matrix
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rowSize = matrix.size(),colSize = matrix[0].size();
+
+        int start = 0;int end = rowSize*colSize-1;
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            int rowIndex = mid/colSize;
+            int colIndex = mid%colSize;
+
+            if(matrix[rowIndex][colIndex]==target){
+                return true;
+            }
+            else if(matrix[rowIndex][colIndex]<target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+};
