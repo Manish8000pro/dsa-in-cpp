6f0aded1d073e71efb74f48ae46d4d6f43d3dcdb
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-09 15:55:49 +0530

    writing class solu.

--- Practice Questions/440. Minimum Spanning Tree.cpp
+++ Practice Questions/440. Minimum Spanning Tree.cpp
@@ -1,1 +1,12 @@
 // 440. Minimum Spanning Tree 
+class Solution {
+public:
+    vector<int>parent;
+    vector<int>size;
+
+    int find(int x){
+        if(parent[x]==x)
+        return x;
+
+        return parent[x] = find(parent[x]);
+    }
