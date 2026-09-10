a6b893c49fd6eb9b7d9a59ece04a21ade2578448
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-10 21:03:56 +0530

    brute force solution

--- Practice Questions/447. Count number of hops.cpp
+++ Practice Questions/447. Count number of hops.cpp
@@ -1,1 +1,18 @@
 //447. Count number of hops
+class Solution {
+public:
+    long long countWays(int n) {
+        // code here
+        if (n == 0) 
+        return 1;
+
+        if (n == 1) 
+        return 1;
+
+        if (n == 2) 
+        return 2;
+        
+        return countWays(n-1) + countWays(n-2) + countWays(n-3);
+
+    }
+};
