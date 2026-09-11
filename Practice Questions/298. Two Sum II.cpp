d7be8530f9828a6dd62f5e87b203ab725709ded3
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-11 14:06:59 +0530

    code

--- Practice Questions/298. Two Sum II.cpp
+++ Practice Questions/298. Two Sum II.cpp
@@ -1,3 +1,17 @@
 //298. Two Sum II
 // this is the brute force approach
-// This approach gives the TLE On LeetCode 
+// This approach gives the TLE On LeetCode
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //code here
+        for(int i = 0;i<nums.size();i++){
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i+1,j+1};
+                }
+            }
+        }
+        return {};
+    }
+}; 
