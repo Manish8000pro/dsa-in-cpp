cc9ac4743b9572b551891f1375d09c0946816b62
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-11 14:23:42 +0530

    Optimized code and solution

--- Practice Questions/298. Two Sum II second sol.cpp
+++ Practice Questions/298. Two Sum II second sol.cpp
@@ -1,3 +1,26 @@
 //298. Two Sum II second solution
 // Time Complexity O(n)
 // Space COmplexity
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //code here
+        int left = 0;
+        int right = nums.size()-1;
+
+        while(left<right){
+            int sum = nums[left]+nums[right];
+
+            if(sum==target){
+                return {left+1,right+1};
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};
+    }
+};
