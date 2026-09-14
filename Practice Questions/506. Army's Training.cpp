830a2ebc7b46d8c69906e55f5c1baeadfeb478b4
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-14 16:54:48 +0530

    Brute force code

--- Practice Questions/506. Army's Training.cpp
+++ Practice Questions/506. Army's Training.cpp
@@ -2,3 +2,30 @@
 //Time complexity O(2^n);
 // space complexity o(n);
 // This gives the TLE becasuse This is not Optimized solution
+class Solution {
+public:
+    int solve(int day,int last,vector<vector<int>>&points,int n){
+        // All days completed 
+        if(day==n){
+            return 0;
+        }
+
+        int ans = 0;
+        // try all 3 activites
+        for(int activity = 0;activity<3;activity++){
+            
+            // cannot repeat the last activity 
+            if(activity!=last){
+                int score = points[day][activity]+solve(day+1,activity,points,n);
+
+                ans = max(ans,score);
+            }
+        }
+        return ans;
+    }
+
+    int maximumPoints(vector<vector<int>>& points, int n) {
+        // Your code here
+        return solve(0,3,points,n);
+    }
+};
