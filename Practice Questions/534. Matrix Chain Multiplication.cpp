83c90f56c25efbfc677e2336805fb858ef142fa5
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-17 17:51:20 +0530

    Code of brute force

--- Practice Questions/534. Matrix Chain Multiplication.cpp
+++ Practice Questions/534. Matrix Chain Multiplication.cpp
@@ -1,2 +1,26 @@
 //534. Matrix Chain Multiplication  
 // This gives  Time limitexceed 
+class Solution {
+public:
+    int solve(int i ,int j ,vector<int>&arr){
+        // only one matrix
+        if(i==j)
+        return 0;
+
+        int ans = INT_MAX;
+
+        // Try every possible partition 
+        for(int k=i;k<j;k++){
+            int cost = (solve(i,k,arr)+solve(k+1,j,arr)+arr[i-1]*arr[k]*arr[j]);
+
+            ans = min(ans,cost);
+        }
+        return ans;
+    }
+
+    int matrixMultiplication(vector<int>& arr) {
+        // Your code here
+        int n = arr.size();
+        return solve(1,n-1,arr);
+    }
+};
