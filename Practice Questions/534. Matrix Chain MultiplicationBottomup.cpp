44b7cb460de8edfed8fc8d9dd98cf8fb127685f6
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-18 17:09:23 +0530

    bottom up approach

--- Practice Questions/534. Matrix Chain MultiplicationBottomup.cpp
+++ Practice Questions/534. Matrix Chain MultiplicationBottomup.cpp
@@ -1,1 +1,30 @@
 //534. Matrix Chain Multiplication
+class Solution {
+public:
+    int matrixMultiplication(vector<int>& arr) {
+        // Your code here
+
+        int n = arr.size();
+
+        vector<vector<int>>dp(n,vector<int>(n,0));
+
+        // length = number of matrices in the interval
+
+        for(int len = 2;len<=n-1;len++){
+            for(int i=1;i+len-1<=n-1;i++){
+                
+                int j = i+len-1;
+
+                dp[i][j] = INT_MAX;
+
+                for(int k=i;k<j;k++){
+                    int cost = dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
+
+                    dp[i][j] = min(dp[i][j],cost);
+                }
+            }
+        }
+        return dp[1][n-1];
+
+    }
+};
