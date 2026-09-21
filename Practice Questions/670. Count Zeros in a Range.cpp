009ab37dda273a90d2437a37ab24920dc0211b57
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-21 18:53:04 +0530

    segment Tree

--- Practice Questions/670. Count Zeros in a Range.cpp
+++ Practice Questions/670. Count Zeros in a Range.cpp
@@ -1,2 +1,18 @@
 //670. Count Zeros in a Range 
 // learning 
+class SegmentTree {
+public:
+    vector<int> tree;
+    int n;
+
+    // Constructor
+    SegmentTree(vector<int>& arr) {
+        n = arr.size();
+
+        tree.resize(4 * n);
+
+        build(0, 0, n - 1, arr);
+    }
+
+    // Build Segment Tree
+    void build(int node, int start, int end, vector<int>& arr) {
