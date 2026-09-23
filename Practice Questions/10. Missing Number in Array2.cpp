7f70cadc7b1ac3535f1c4b05f9b9e20be24cf178
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-23 17:45:30 +0530

    Optimized code

--- Practice Questions/10. Missing Number in Array2.cpp
+++ Practice Questions/10. Missing Number in Array2.cpp
@@ -1,1 +1,14 @@
 //10. Missing Number in Array Second approach 
+int findMissingPositive(vector<int>& arr) {
+    // Your code here
+    sort(arr.begin(),arr.end());
+
+    int missing = 1;
+
+    for(int num: arr){
+        if(num==missing){
+            missing++;
+        }
+    }
+    return missing;
+}
