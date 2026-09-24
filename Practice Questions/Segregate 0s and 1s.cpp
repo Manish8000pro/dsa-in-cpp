77dbb3b2faf4e4884731956cb7e0f5181797cc19
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-24 16:59:14 +0530

    solved code

--- Practice Questions/Segregate 0s and 1s.cpp
+++ Practice Questions/Segregate 0s and 1s.cpp
@@ -1,1 +1,17 @@
 //36. Segregate 0s and 1s
+void segregate0and1(vector<int>& arr) {
+    // Your code here
+    int count0 = 0;
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+        if(arr[i]== 0)
+        count0++;
+    }
+
+    for(int i=0;i<count0;i++){
+        arr[i] = 0;
+    }
+    for(int i=count0;i<n;i++){
+        arr[i] = 1;
+    }
+}
