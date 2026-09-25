e86c5c225d5067ff6d203eab0e756f35bc8b3b9a
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-25 15:43:54 +0530

    solved

--- Practice Questions/40. Remove Duplicates from Sorted Array.cpp
+++ Practice Questions/40. Remove Duplicates from Sorted Array.cpp
@@ -1,1 +1,18 @@
 //40. Remove Duplicates from Sorted Array
+int removeDuplicates(vector<int>& arr) {
+    // Your code here
+    int n  = arr.size();
+    if(n==0)
+    return 1;
+    
+    int count = 1;
+
+    for(int i=1;i<n;i++){
+        if(arr[i]==arr[i-1])
+        continue;
+        else{
+            count++;
+        }
+    }
+    return count;
+}
