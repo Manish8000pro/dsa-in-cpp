b3f4841ef5e85d1c716a2fe6e7dd247ae36d21e0
Manish8000pro <manishkumawat6227@gmail.com>
2026-09-26 17:01:26 +0530

    code of this problem

--- Practice Questions/43. Maximum Prefix Sum for a Given Range.cpp
+++ Practice Questions/43. Maximum Prefix Sum for a Given Range.cpp
@@ -1,2 +1,24 @@
 //43. Maximum Prefix Sum for a Given Range
 // solving with Brute force app
+vector<int> maxPrefixes(vector<int>& arr, vector<int>& leftIndex, vector<int>& rightIndex) {
+    // Your code here
+    vector<int>answer;
+
+    for(int q=0;q<leftIndex.size();q++){
+
+        int L = leftIndex[q];
+        int R = rightIndex[q];
+
+        int sum = 0;
+        int ans = INT_MIN;
+
+        for(int i = L; i <= R; i++) {
+
+            sum += arr[i];
+
+            ans = max(ans, sum);
+        }
+        answer.push_back(ans);
+    }
+    return answer;
+}
